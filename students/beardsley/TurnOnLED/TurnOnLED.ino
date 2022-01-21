b4329da9daad99d50fd6c15415688f94a1558745
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-01-21 12:11:13 -0600

    added missing semicolon

--- students/beardsley/TurnOnLED/TurnOnLED.ino
+++ students/beardsley/TurnOnLED/TurnOnLED.ino
@@ -3,7 +3,7 @@
 const int LED=9;  // Define LED for pin 9
 
 void setup() {
-  pinMode(LED, OUTPUT)  // Set the LED pin as an output
+  pinMode(LED, OUTPUT);  // Set the LED pin as an output
   digitalWrite(LED, HIGH);  // Set the LED pin high
 }
 
