8fea128b12d1e48d32d3e6fcdf2c5c9394977de8
Jonathan Hasan <jonh.hasan@gmail.com>
2022-02-04 00:04:55 -0600

    Uploaded initial pwm exploration with int arguments
    pwm has int arguments - Error occurs where incorrect value is output for different times

---
+++ students/Hasan/Lab1/Part2_PWMExploration/pwm_exploration/pwm_exploration.ino
@@ -0,0 +1,65 @@
+const int yLED = 9; 
+
+
+/*
+ * Write a PWM function
+ * Inputs:
+ *  PWM value corresponds to brightness
+ *  `o for off, 255 for brightest setting 
+ *  Pin Number
+ *  period
+ *    In microseconds 
+ *  
+ *  Outputs: 
+ *    none 
+ */
+
+void pwm(int pin, int brightness, int period) {
+  //Calculate Ton
+
+  int Ton; 
+  int Toff; 
+  
+  Ton = brightness * period/255; 
+  
+  //Calculate Toff 
+
+  Toff = period = Ton;
+  
+  // Turn on 
+
+  digitalWrite(pin, HIGH); 
+
+  //  delay for t on 
+  delayMicroseconds(Ton); 
+
+  // Turn off 
+
+  DigitalWrite(pin, LOW); 
+  //delay for t off 
+
+  delayMicroseconds(Toff); 
+  
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // set pin mode to output 
+  pinMode(yLED, OUTPUT); 
+
+  pinMode(bLED, OUTPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int brightness = 100; 
+  int period = 2040; 
+
+  //set yLED brightness
+  analogWrite(yLED, 100);
+  //call self-make
+  pwm(bLED, brightness, period); 
+
+}
