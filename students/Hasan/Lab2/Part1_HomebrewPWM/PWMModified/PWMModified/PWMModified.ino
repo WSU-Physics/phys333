576a0e7c94f0dcdc7aab689a9b219e9b83782bc4
Jonathan Hasan <jonh.hasan@gmail.com>
2022-02-11 12:32:29 -0600

    Create PWMModified.ino
    First version of PWM code, Professor beardsley brought boolean logic to my attention which solved the issue of incorrect PWM outputs.  In addition, code creates period of 52 us which is incorrect.

---
+++ students/Hasan/Lab2/Part1_HomebrewPWM/PWMModified/PWMModified/PWMModified.ino
@@ -0,0 +1,52 @@
+const int gLED = 10; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(gLED, OUTPUT); 
+}
+
+void pwm(int pin, double duty_cycle, double duration) {
+
+// In seconds
+  double Current_time = millis()/1000; 
+
+  // Check if the time is less than the duration 
+  if (Current_time < duration){
+      //Calculate Ton
+      
+  double Ton; 
+  //Calculate Toff
+  double Toff; 
+
+  Ton = duty_cycle * 10/100.0; 
+
+  Toff = abs(Ton - (10/100.0)); 
+
+
+ //boolean logic
+  digitalWrite(pin, duty_cycle && HIGH); 
+
+  delayMicroseconds(Ton); 
+
+  digitalWrite(pin, duty_cycle && LOW); 
+
+  delayMicroseconds(Toff); 
+
+  
+
+  
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  double duration = 10; 
+  
+  double dutycycle = .5; 
+
+  pwm(gLED, dutycycle, duration ); 
+
+}
