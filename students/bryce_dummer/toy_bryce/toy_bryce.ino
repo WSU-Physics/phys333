dffb5fa62030a67c57815c46f9f471ef0d938107
BryceWD23 <BryceDummer2001@gmail.com>
2022-02-25 13:23:50 -0600

    version 1

--- students/bryce_dummer/toy_bryce/toy_bryce.ino
+++ students/bryce_dummer/toy_bryce/toy_bryce.ino
@@ -1,4 +1,4 @@
-// POV toy demo framework //
+
 
 // ------- Preamble -------- //
 #include <avr/io.h>
@@ -7,25 +7,26 @@
 // -------- Functions --------- //
 void POVDisplay(uint8_t oneByte) {
   PORTD = oneByte;
-  _delay_ms(200);
+  _delay_ms(100);//speed
+}
+void POVDisplay2(uint8_t oneByte) {
+  PORTD = oneByte;
+  _delay_ms(100);//speed
 }
 
 int main(void) {
+
   // -------- Inits --------- //
   DDRD = 0xff;                    /* Set up all of LED pins for output */
   // ------ Event loop ------ //
   while (1) {                                              /* mainloop */
-    POVDisplay(0b00000011);
-    POVDisplay(0b00001100);
-    POVDisplay(0b00110000);
-    POVDisplay(0b01100000);
-    POVDisplay(0b11000000);
-    POVDisplay(0b01100000);
-    POVDisplay(0b00110000);
-    POVDisplay(0b00001100);
-    POVDisplay(0b00000011);
+    POVDisplay(0b10001000);
+    POVDisplay(0b10000100);
+    POVDisplay(0b00000010);
+    POVDisplay(0b00000001);           
     PORTD = 0;
-    _delay_ms(100);
-  }                                                    /* end mainloop */
+
+
+  }
   return 0;
 }
