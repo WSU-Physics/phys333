8949baed90366ecb887d02e6e76ebaf86561585d
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-03-02 10:34:07 -0600

    transmit arbitrary message

--- students/beardsley/diy_uart/diy_uart.c
+++ students/beardsley/diy_uart/diy_uart.c
@@ -3,7 +3,8 @@
  * a UART interface?
  * Goals for this code:
  * X transmit a single character "a"
- * - Create function to transmit arbitrary byte 
+ * X Create function to transmit arbitrary byte 
+ * - Create function to transmit arbitrary message
  * - Read a byte, loopback to computer
  */
 
@@ -19,37 +20,46 @@
 #define setBit(byte, bit)     (byte |= BV(bit))  // set bit within byte
 #define clearBit(byte, bit)   (byte &= ~BV(bit)) // clear bit
 
+
+void transmitByte(uint8_t byte, int sdelay_us){
+  uint8_t mask;
+  uint8_t currState;
+  clearBit(SERIAL_PORT, TX); // start bit
+  _delay_us(sdelay_us);
+
+  for (int i=0; i<8; i++){
+    mask = (byte >> i);  // Shift relevant bit to LSB
+    mask = (mask << TX); // Shift to TX bit
+    mask &= (1 << TX);  // Set rest of bits to 0
+    currState = SERIAL_PORT;
+    currState = clearBit(currState, TX);
+    SERIAL_PORT = currState | mask;
+    _delay_us(sdelay_us);
+  }
+
+  setBit(SERIAL_PORT, TX); // Stop bit
+  _delay_us(sdelay_us);
+}
+
+void transmitMessage(char message[], int sdelay_us){
+    int i = 0;
+    while(message[i] != '\0'){
+      transmitByte(message[i++], sdelay_us);
+    }
+}
+
+
 int main(void){
 
   int baud = 9600;  // Slow baud rate
   int sdelay_us = 1000000 / baud;  // delay between serial bits, in microseconds
+  char message[] = "Hello World!";
 
   SERIAL_DDR ^= (1 << TX);
 
   while (1){
-    // transmit letter a = 0b01100001
-    // note it should be transmitted LSB first
-    clearBit(SERIAL_PORT, TX); // start bit
-    _delay_us(sdelay_us);
-    setBit(SERIAL_PORT, TX); // 1
-    _delay_us(sdelay_us);
-    clearBit(SERIAL_PORT, TX); // 0
-    _delay_us(sdelay_us);
-    clearBit(SERIAL_PORT, TX); // 0
-    _delay_us(sdelay_us);
-    clearBit(SERIAL_PORT, TX); // 0
-    _delay_us(sdelay_us);
-    clearBit(SERIAL_PORT, TX); // 0
-    _delay_us(sdelay_us);
-    setBit(SERIAL_PORT, TX); // 1
-    _delay_us(sdelay_us);
-    setBit(SERIAL_PORT, TX); // 1
-    _delay_us(sdelay_us);
-    clearBit(SERIAL_PORT, TX); // 0
-    _delay_us(sdelay_us);
-    setBit(SERIAL_PORT, TX); // Stop bit
-    _delay_us(sdelay_us);
-    
+
+    transmitMessage(message, sdelay_us);    
     _delay_ms(1000);  // wait a second
   }
 
