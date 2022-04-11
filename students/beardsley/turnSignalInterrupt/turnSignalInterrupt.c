8e838c0556f51495a298b69f3783871c04f0f14f
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-04-11 12:52:58 -0500

    turn signal now uses pin change interrupt

--- students/beardsley/turnSignalInterrupt/turnSignalInterrupt.c
+++ students/beardsley/turnSignalInterrupt/turnSignalInterrupt.c
@@ -10,6 +10,7 @@
 // ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #define DEBOUNCE_TIME  5                               /* microseconds */
 #define LED_DDR DDRD
 #define LED_PORT PORTD
@@ -21,6 +22,15 @@
 
 uint8_t buttonPressedR = 0;
 uint8_t buttonPressedL = 0;
+int state = 0;
+
+// TODO:
+// x include the interrupt library
+// x define ISR 
+// x enable specific interrupts, define pins
+// x enable sei
+// x revisit main loop logic
+// x promote state variable
 
 uint8_t debouncePress(button) {
   if (bit_is_clear(BUTTON_PIN, button)) {      /* button is pressed now */
@@ -55,16 +65,51 @@ uint8_t buttonCheck(void) {
   return (0);
 }
 
+ISR(PCINT0_vect){
+  // handle the interrupt
+  int result = buttonCheck(); // return 1 if R, 2 if L, 0 if neither
+  // if Right button -> animate to the right? or turn off if already animating
+  if (result == 1){
+    // Right button pushed
+    if (state == 1){
+      // Already turning right, now turn off
+      state = 0;
+    } else {
+      // Start turning right
+      state = 1;
+    }
+  }
+  // similar for left
+  if (result == 2){
+    // Left button pushed
+    if (state == 2){
+      // Already turning left, now turn off
+      state = 0;
+    } else {
+      // Start turning left
+      state = 2;
+    }
+  }
+  // nothing if neither
+}
+
+void interrupt_init(void) {
+  // using arduino 8 & 9 -> PB0, PB1 for BUTTONR and BUTTONL
+  // enable specific interrupts, define pins
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= ((1 << PCINT0) | (1 << PCINT1));
+  // enable global interrupt
+  sei();
+}
+
 int main(void) {
   // -------- Inits --------- //
   uint8_t buttonWasPressed=0;                                 /* state */
   BUTTON_PORT |= ((1 << BUTTONR) | (1 << BUTTONL));     /* enable the pullup on the button */
   LED_DDR = 0xff;                      /* set up LED for output */
-  int tick = 0;
-  int tick_ms = 10;
-  int delay_ticks = 20;
-  int state = 0;
-  int last_change = 0;
+  int delay_time = 200;
+
+  interrupt_init();
 
 
   // ------ Event loop ------ //
@@ -73,47 +118,33 @@ int main(void) {
     switch (state) {
       case 1:
         // Light LEDs to the right
-        if (tick - last_change >= delay_ticks){
-          if (LED_PORT == 0){
-            LED_PORT = (1 << 3);
-          }
-          else if (LED_PORT == 0x0f){
-            LED_PORT = 0;
-          } else{
-            LED_PORT |= (LED_PORT >> 1);
-          }
-          last_change = tick;
+        if (LED_PORT == 0){
+          LED_PORT = (1 << 3);
         }
-        
-        if (buttonCheck() == 1){
-          state = 0;
+        else if (LED_PORT == 0x0f){
+          LED_PORT = 0;
+        } else{
+          LED_PORT |= (LED_PORT >> 1);
         }
+
         break;
       case 2:
         // Light LEDs to the left
-        if (tick - last_change >= delay_ticks){
-          if (LED_PORT == 0){
-            LED_PORT = (1 << 4);
-          }
-          else if (LED_PORT == 0xf0){
-            LED_PORT = 0;
-          } else{
-            LED_PORT |= (LED_PORT << 1);
-          }
-          last_change = tick;
+        if (LED_PORT == 0){
+          LED_PORT = (1 << 4);
+        }
+        else if (LED_PORT == 0xf0){
+          LED_PORT = 0;
+        } else{
+          LED_PORT |= (LED_PORT << 1);
         }
         
-        if (buttonCheck() == 2){
-          state = 0;
-        }
         break;
       default:
         // Off
         LED_PORT = 0;
-        state = buttonCheck();
     }
-    _delay_ms(tick_ms);
-    tick++;
+    _delay_ms(delay_time);
 
 
   }                                                  /* End event loop */
