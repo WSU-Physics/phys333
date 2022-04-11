68975a59d951d91e8648607e064613716ee89ea2
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-04-11 09:17:45 -0500

    turn signal activity to use interrupts

---
+++ students/beardsley/turnSignalInterrupt/turnSignalInterrupt.c
@@ -0,0 +1,112 @@
+// ------- Preamble -------- //
+#include <avr/io.h>
+#include <util/delay.h>
+#define DEBOUNCE_TIME  5                               /* microseconds */
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+#define BUTTON_DDR DDRB
+#define BUTTON_PORT PORTB
+#define BUTTON_PIN PINB
+#define BUTTONR PB0
+#define BUTTONL PB1
+
+uint8_t buttonPressedR = 0;
+uint8_t buttonPressedL = 0;
+
+uint8_t debouncePress(button) {
+  if (bit_is_clear(BUTTON_PIN, button)) {      /* button is pressed now */
+    _delay_ms(DEBOUNCE_TIME);
+    if (bit_is_clear(BUTTON_PIN, button)) {            /* still pressed */
+      return (1);
+    }
+  }
+  return 0;
+}
+
+uint8_t buttonCheck(void) {
+  // Check if button is pushed, return 1 if Right, 2 if Left, 0 otherwise
+  if (debouncePress(BUTTONR)){
+    if (buttonPressedR == 0){
+      buttonPressedR = 1;
+      return (1);
+    }
+  } else {
+    buttonPressedR = 0;
+  }
+
+  if (debouncePress(BUTTONL)){
+    if (buttonPressedL == 0){
+      buttonPressedL = 1;
+      return (2);
+    }
+  } else {
+    buttonPressedL = 0;
+  }
+
+  return (0);
+}
+
+int main(void) {
+  // -------- Inits --------- //
+  uint8_t buttonWasPressed=0;                                 /* state */
+  BUTTON_PORT |= ((1 << BUTTONR) | (1 << BUTTONL));     /* enable the pullup on the button */
+  LED_DDR = 0xff;                      /* set up LED for output */
+  int tick = 0;
+  int tick_ms = 10;
+  int delay_ticks = 20;
+  int state = 0;
+  int last_change = 0;
+
+
+  // ------ Event loop ------ //
+  while (1) {
+
+    switch (state) {
+      case 1:
+        // Light LEDs to the right
+        if (tick - last_change >= delay_ticks){
+          if (LED_PORT == 0){
+            LED_PORT = (1 << 3);
+          }
+          else if (LED_PORT == 0x0f){
+            LED_PORT = 0;
+          } else{
+            LED_PORT |= (LED_PORT >> 1);
+          }
+          last_change = tick;
+        }
+        
+        if (buttonCheck() == 1){
+          state = 0;
+        }
+        break;
+      case 2:
+        // Light LEDs to the left
+        if (tick - last_change >= delay_ticks){
+          if (LED_PORT == 0){
+            LED_PORT = (1 << 4);
+          }
+          else if (LED_PORT == 0xf0){
+            LED_PORT = 0;
+          } else{
+            LED_PORT |= (LED_PORT << 1);
+          }
+          last_change = tick;
+        }
+        
+        if (buttonCheck() == 2){
+          state = 0;
+        }
+        break;
+      default:
+        // Off
+        LED_PORT = 0;
+        state = buttonCheck();
+    }
+    _delay_ms(tick_ms);
+    tick++;
+
+
+  }                                                  /* End event loop */
+  return 0;                            /* This line is never reached */
+}
