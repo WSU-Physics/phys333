d4ecb8665face4347ee672335f1aee2c624faaec
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-04-24 15:13:34 -0500

    header file for songs

---
+++ students/beardsley/backgroundMusic/songs.h
@@ -0,0 +1,21 @@
+/*
+ * Define some songs to play
+ */
+
+#define MARYNOTES {E5, D5, C5, D5, E5, E5, E5, REST,\
+               D5, D5, D5, REST, E5, G5, G5, REST,\
+               E5, D5, C5, D5, E5, E5, E5, E5,\
+               D5, D5, E5, D5, C5, REST}
+/* 120 bpm = 500 ms per 1/4 note*/
+#define MARYLENGTHS {500, 500, 500, 500, 500, 500, 500, 500,\
+                 500, 500, 500, 500, 500, 500, 500, 500,\
+                 500, 500, 500, 500, 500, 500, 500, 500,\
+                 500, 500, 500, 500, 1000, 1000}
+
+#define MARIONOTES {E5, E5, E5, REST, C5, E5, G5 REST, G4, REST,\
+                C5, REST, G4, REST, E4, REST, A5, REST, B5, REST, Ax5, A5,\
+                G4, E5, G5, A6, F5, G5, REST, E5, REST, C5, D5, B5, REST}
+/* 90 bpm ~ 668 ms per 1/4 note */
+#define MARIOLENGTHS {167, 334, 167, 167, 167, 334, 334, 334, 334, 334,]
+                334, 167, 167, 334, 334, 167, 167, 167, 167, 167, 167, 334,\
+                223, 223, 223, 334, 167, 167, 167, 167, 167, 167, 167, 167, 334}
