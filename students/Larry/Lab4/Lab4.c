15ef6678265be10f135a80a51ceceeaf4df838d3
Larry-vt <80938629+Larry-vt@users.noreply.github.com>
2022-04-30 03:37:04 -0500

    Lab 4 edited

--- students/Larry/Lab4/Lab4.c
+++ students/Larry/Lab4/Lab4.c
@@ -5,16 +5,19 @@
 #define BUTTON PC4
 #define BUTTON_PORT PORTC
 #define BUTTON2 PC5
+
+
 //#define BUTTON
 
+  uint8_t buttonWasPressed = 0;
+  uint8_t button2WasPressed = 0;
 
-
-uint8_t debounce (void){
-  if(bit_is_clear(BUTTON_PIN, BUTTON)){
+uint8_t debounce (button){
+  if(bit_is_clear(BUTTON_PIN, button)){
     
     _delay_us(DEBOUNCE_TIME);
     
-    if(bit_is_clear(BUTTON_PIN, BUTTON)){
+    if(bit_is_clear(BUTTON_PIN, button)){
       
       return(1);
       
@@ -23,7 +26,7 @@ uint8_t debounce (void){
   
   return(0);
 }
-
+/*
 uint8_t debounce2 (void){
   if(bit_is_clear(BUTTON_PIN, BUTTON2)){
     
@@ -38,47 +41,89 @@ uint8_t debounce2 (void){
   
   return(0);
 }
+*/
+uint8_t buttonPressed(void){
+  if(debounce(BUTTON)){
+    return(1);
+    
+  }
+  if(debounce(BUTTON2)){
+    return(2);
+  }
+  
+
+  /*
+  if(bit_is_clear(BUTTON_PIN2, BUTTON2 )){
+    _delay_us(DEBOUNCE_TIME);
+    if(bit_is_clear(BUTTON_PIN2, BUTTON2)){
+      return(2);
+    
+    }
+  }
+  if(bit_is_clear(BUTTON_PIN, BUTTON )){
+    _delay_us(DEBOUNCE_TIME);
+    if(bit_is_clear(BUTTON_PIN, BUTTON)){
+      return(1);
+    }
+    */
+  
+
+    return(0);
+ 
+  
+}
 
 int main(void) {
-  uint8_t buttonWasPressed;
-  uint8_t button2WasPressed;
-  BUTTON_PORT |= (1<< BUTTON);
-  BUTTON_PORT |= (1<< BUTTON2);
+
+  BUTTON_PORT |= ((1<< BUTTON) | (1<< BUTTON2));
+  //BUTTON_PORT2 |= (1<< BUTTON2);
+  //int b = buttonPressed(BUTTON2);
+  //int b2 = buttonPressed(BUTTON);
   
 
   DDRD = 0b11111111;
-  //DDRD = 0b01111000;
+  DDRC = 0b00000000;
   
 
 
   while(1){
-    if(debounce()){
-      if(buttonWasPressed ==0){      
-    
-    PORTD = 0b11110000;
+    if(buttonPressed() == 1){
+      if(buttonWasPressed == 0){ 
+        if(PORTD == 0b00000000){
+               
+          PORTD = 0b11110000;
+        }
+        else{
+          PORTD = 0b00000000;
+        }
 
-    buttonWasPressed =1;
+    buttonWasPressed = 1;
       }
       else{
         buttonWasPressed = 0;
-        //PORTD = 0b00000000;
+        PORTD = 0b00000000;
         
       }
     }
 
-    if(debounce2()){
-      button2WasPressed = 0;
+   if(buttonPressed() == 2){
+      //button2WasPressed = 0;
       if(button2WasPressed == 0){
+        if(PORTD == 0b00000000){
 
         PORTD = 0b00001111;
-        
+        }
+        else{
+          PORTD = 0b00000000;
+          
+        }
         
 
         button2WasPressed = 1;
       }
       else{
         button2WasPressed = 0;
-        //PORTD = 0b00000000;
+        PORTD = 0b00000000;
       }
      
     }
@@ -87,6 +132,6 @@ int main(void) {
 
     return(0);
   }
-//return (0);
+return (0);
 
 }
