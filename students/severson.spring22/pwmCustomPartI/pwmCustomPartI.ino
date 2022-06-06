dc0d328fb0a9159cf74c09170f16ad58cbc5627d
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-06 14:48:45 -0500

    lab02, part I, update
    Just made the function more general.

--- students/severson.spring22/pwmCustomPartI/pwmCustomPartI.ino
+++ students/severson.spring22/pwmCustomPartI/pwmCustomPartI.ino
@@ -1,30 +1,30 @@
 /*
-   A custom PWM function capable of greater precision than analogWrite()
+   A custom PWM function capable of greater precision than analogWrite().
 */
 
-const int ledBlue = 2;  //set pin 2 as ledBlue
+const int ledPin = 2;  //set non-PWM pin as ledPin
+const double T = 10000.;  //sets the period to 10000 (microseconds)
 
 void pwm(int pin, double dutyCycle, double duration) { //custom pwm function with the arguments pin, dutyCycle, and duration
-  const double T = 10000.;  //sets the period to 10000 (microseconds)
-  double Ton = dutyCycle * T; //calculate time ledBlue is on
-  double Toff = T - Ton;  //calculate time ledBlue is off
+  double Ton = dutyCycle * T; //calculate time ledPin is on
+  double Toff = T - Ton;  //calculate time ledPin is off
   double nLoops = duration / T; //number of loops needed to run during a full duration
 
   for (int i = 0.; i < nLoops; i++) { //incrementing the number of loops from zero to nLoops
-    digitalWrite(ledBlue, HIGH);  //set ledBlue high
-    delayMicroseconds(Ton); //ledBlue is on
-    digitalWrite(ledBlue, LOW); //set ledBlue low
-    delayMicroseconds(Toff);  //ledBlue is off
+    digitalWrite(ledPin, HIGH);  //set ledPin high
+    delayMicroseconds(Ton); //ledPin is on
+    digitalWrite(ledPin, LOW); //set ledPin low
+    delayMicroseconds(Toff);  //ledPin is off
   }
 }
 
 void setup() {  //setup function
-  pinMode(ledBlue, OUTPUT); //set pin 2 as output
+  pinMode(ledPin, OUTPUT); //set ledPin as output
 }
 
 void loop() { //main loop
-  pwm(ledBlue, .33, 1000000.);  //calling the custom pwm function setting the duty cycle between 0.0 (off) and 1.0 (full brightness) and setting the duration value in microseconds
-  digitalWrite(ledBlue, LOW); //turn ledBlue off after a full duration
-  delay(1000);  //delay (in milliseconds) before repeating the loop, can be adjusted as desired
+  pwm(ledPin, .3333, 1000000.);  //calling the custom pwm function setting the duty cycle between 0.0 (off) and 1.0 (full brightness) and setting the duration value in microseconds
+  digitalWrite(ledPin, LOW); //turn ledPin off after a full duration
+  delay(3000);  //delay (in milliseconds) before repeating the loop, can be adjusted as desired
 
 }
