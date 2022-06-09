2ac697096fb25f12b6b3348180f95b42b3cf1938
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-09 11:04:11 -0500

    update2 lab03Test.c
    annotated the program

--- students/severson.spring22/lab03Test.c/lab03Test.c.ino
+++ students/severson.spring22/lab03Test.c/lab03Test.c.ino
@@ -1,14 +1,17 @@
-#include <avr/io.h>
-#include <util/delay.h>
+/*
+ * A simple program to light up eight LEDs sequentially.
+ */
 
-int main(void) {
-  DDRD = 0xff;
+#include <avr/io.h>                     //fetch avr/io.h library
+#include <util/delay.h>                 //fetch util/delay.h library
 
-  while (1) {
-    for (uint8_t i = 0; i < 8; i++) {
-      PORTD = (1 << i);
-      _delay_ms(100);
+int main(void) {                        //declare main function
+  DDRD = 0xff;                          //enable all port D pins as output
+
+  while (1) {                           //while true, run this program.
+    for (uint8_t i = 0; i < 8; i++) {   //for loop, initially set i = 0. check if i < 8. if yes run program then update i to i+1. if no set i = 0.
+      PORTD = (1 << i);                 //bit shifting portd
+      _delay_ms(100);                   //delay for designated time
     }
   }
-
 }
