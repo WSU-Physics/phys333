372d0ae7afb5bc9b9e13ef95deb5ce90647cbae3
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-15 13:39:18 -0500

    update lab04, part II
    annotated and fixed a couple errors

--- students/severson.spring22/lab04PartII.c/lab04PartII.c.ino
+++ students/severson.spring22/lab04PartII.c/lab04PartII.c.ino
@@ -1,43 +1,47 @@
 /*
-   Left turn right turn patterns.
+   Left signal right signal patterns.
 */
 
-#include <avr/io.h>
-#include <util/delay.h>
+#include <avr/io.h>               //fetch avr/io library
+#include <util/delay.h>           //fetch util/delay library
 
-#define signalDelay 125
+#define signalDelay 125           //define signal delay time milliseconds
 
-#define signalDDR  DDRD
-#define signalPort PORTD
-#define signalPin  PIND
+#define signalDDR  DDRD           //ddrd used for signals
+#define signalPort PORTD          //portd used for signals
+#define signalPin  PIND           //pind used for signals
 
-uint8_t leftSignal(void) {
-  uint8_t L;
-  signalPort = 0x00;
-  _delay_ms(signalDelay);
+uint8_t leftSignal(void) {        //left signal function
+  uint8_t L;                      //variable for the function
+  signalPort = 0x00;              //clearing the port
+  _delay_ms(signalDelay);         //delay
 
-  for (L = 3; L < 255; L--) {
-    signalPort |= (1 << L);
-    _delay_ms(signalDelay);
+  for (L = 3; L < 255; L--) {     //for loop
+    signalPort |= (1 << L);       //pattern in which the LEDs light up
+    _delay_ms(signalDelay);       //delay
   }
 }
 
-uint8_t rightSignal(void) {
-  uint8_t R;
-  signalPort = 0x00;
-  _delay_ms(signalDelay);
+uint8_t rightSignal(void) {       //right signal function
+  uint8_t R;                      //variable for function
+  signalPort = 0x00;              //clear signal port
+  _delay_ms(signalDelay);         //delay
 
-  for (R = 4; R < 8; R++) {
-    signalPort |= (1 << R);
-    _delay_ms(signalDelay);
+  for (R = 4; R < 8; R++) {       //for loop
+    signalPort |= (1 << R);       //right signal pattern
+    _delay_ms(signalDelay);       //delay
   }
 }
 
-int main(void) {
-  DDRD = 0xff;
+int main(void) {                  //main function
+  signalDDR = 0xff;               //set LED pins as output
 
-  while (1) {
-    leftSignal();
-    rightSignal();
-  }
+  while (1) {                     //while loop
+    leftSignal();                 //call left signal function
+    signalPort = 0x00;            //clear the port
+    _delay_ms(4*signalDelay);     //delay
+    rightSignal();                //call right signal function
+    signalPort = 0x00;            //clear the port
+    _delay_ms(4*signalDelay);     //delay
+  }                               //repeat
 }
