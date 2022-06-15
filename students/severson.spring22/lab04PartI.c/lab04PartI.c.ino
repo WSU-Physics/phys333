8ff31d061999dbbf362c5256f39cecea76495c86
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-15 12:08:14 -0500

    update lab04, part I
    just formatted it to look a bit cleaner

--- students/severson.spring22/lab04PartI.c/lab04PartI.c.ino
+++ students/severson.spring22/lab04PartI.c/lab04PartI.c.ino
@@ -1,11 +1,11 @@
 /*
- *  The Button Test.
- */
+    The Button Test.
+*/
 
- #include <avr/io.h>                                    //fetches the avr/io library
- #include <util/delay.h>                                //fetches the util/delay library
+#include <avr/io.h>                                     //fetches the avr/io library
+#include <util/delay.h>                                 //fetches the util/delay library
 
- #define debounceTime 1000                              //debounce time in microseconds
+#define debounceTime 1000                               //debounce time in microseconds
 
 #define buttonPort PORTB                                //port b is used for the buttons
 #define buttonPin PINB                                  //pin b is used for the buttons
@@ -17,7 +17,7 @@
 #define leftLED PD0                                     //left LED is on pin 0
 #define rightLED PD7                                    //right LED is on pin 7
 
- uint8_t leftDebounce(void) {                           //debounce function for left button
+uint8_t leftDebounce(void) {                            //debounce function for left button
   if ((buttonPin & (1 << leftButton)) == 0) {           //left button is pressed
     _delay_us(debounceTime);                            //delay
     if ((buttonPin & (1 << leftButton)) == 0) {         //left button still pressed
@@ -25,9 +25,9 @@
     }
   }
   return (0);
- }
- 
- uint8_t rightDebounce(void) {                          //right button debounce function
+}
+
+uint8_t rightDebounce(void) {                           //right button debounce function
   if ((buttonPin & (1 << rightButton)) == 0) {          //right button pressed
     _delay_us(debounceTime);                            //delay
     if ((buttonPin & (1 << rightButton)) == 0) {        //right button still pressed
@@ -35,12 +35,12 @@
     }
   }
   return (0);
- }
+}
 
 int main(void) {                                        //main function
   uint8_t leftButtonPressed;                            //integer for left button state
   uint8_t rightButtonPressed;                           //integer for right button state
-  buttonPort |= (1 << leftButton)|(1 << rightButton);   //enable pullup on buttons
+  buttonPort |= (1 << leftButton) | (1 << rightButton); //enable pullup on buttons
   signalDDR = 0xff;                                     //prepare LEDs for output
 
   while (1) {                                           //loop start
