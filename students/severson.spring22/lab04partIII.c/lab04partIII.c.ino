6c5a3565e3adb25879c8b305fef30ac8912f529b
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-20 15:56:34 -0500

    update lab04part3.c
    trying to put leftTurn() into leftSignal()

--- students/severson.spring22/lab04partIII.c/lab04partIII.c.ino
+++ students/severson.spring22/lab04partIII.c/lab04partIII.c.ino
@@ -20,27 +20,38 @@
 #define signalPort PORTD                                //portd used for signals
 #define signalPin  PIND                                 //pind used for signals
 
+uint8_t leftButtonPressed;                              //integer for left button state
+uint8_t rightButtonPressed;                             //integer for right button state
+
 uint8_t leftSignal(void) {                              //left signal function
   uint8_t l;                                            //variable for the function
-  
-  for (l = 3; l < 255; l--) {                           //for loop
+  leftButtonPressed = 1;
+  if(leftTurn()) {
+    if(leftButtonPressed == 1){
+      for (l = 3; l < 255; l--) {                           //for loop
     signalPort |= (1 << l);                             //pattern in which the LEDs light up
     _delay_ms(signalDelay);                             //delay
+    signalPort = 0x00;
+    leftButtonPressed = 0;
+      }
+  }
+  else {
+    leftButtonPressed = 1;
+  }
+  
   }
 }
 
 uint8_t rightSignal(void) {                             //right signal function
   uint8_t r;                                            //variable for function
-  signalPort = 0x00;                                    //clear signal port
-  _delay_ms(signalDelay);                               //delay
-
   for (r = 4; r < 8; r++) {                             //for loop
     signalPort |= (1 << r);                             //right signal pattern
     _delay_ms(signalDelay);                             //delay
+    signalPort = 0x00;
   }
 }
 
-uint8_t leftTurn(void) {                            //debounce function for left button
+uint8_t leftTurn(void) {                                //debounce function for left button
   if ((buttonPin & (1 << leftButton)) == 0) {           //left button is pressed
     _delay_us(debounceTime);                            //delay
     if ((buttonPin & (1 << leftButton)) == 0) {         //left button still pressed
@@ -50,7 +61,7 @@ uint8_t leftTurn(void) {                            //debounce function for left
   return (0);
 }
 
-uint8_t rightTurn(void) {                           //right button debounce function
+uint8_t rightTurn(void) {                               //right button debounce function
   if ((buttonPin & (1 << rightButton)) == 0) {          //right button pressed
     _delay_us(debounceTime);                            //delay
     if ((buttonPin & (1 << rightButton)) == 0) {        //right button still pressed
@@ -61,37 +72,30 @@ uint8_t rightTurn(void) {                           //right button debounce func
 }
 
 int main(void) {                                        //main function
-  uint8_t leftButtonPressed;                            //integer for left button state
-  uint8_t rightButtonPressed;                           //integer for right button state
   buttonPort |= (1 << leftButton) | (1 << rightButton); //enable pullup on buttons
   signalDDR = 0xff;                                     //prepare LEDs for output
 
   while (1) {                                           //loop start
     if (leftTurn()) {                                   //if left button pressed
       if (leftButtonPressed == 0) {                     //but wasn't previously
+        leftSignal();
         leftButtonPressed = 1;                          //update left button state
-        while(leftButtonPressed == 1) {
-          leftSignal();
-          signalPort = 0x00;
-          _delay_ms(signalDelay);
-        }
       }
     }
     else {                                              //left button not pressed now
       leftButtonPressed = 0;                            //update state
-      signalPort = 0x00;
     }
-}
+  }
 
-    if (rightTurn()) {                                   //everything is similar, just for the right button & LED
-      if (rightButtonPressed == 0) {
-        rightSignal();
-        rightButtonPressed = 1;
-      }
+  if (rightTurn()) {                                   //everything is similar, just for the right button & LEDs
+    if (rightButtonPressed == 0) {
+      rightSignal();
+      rightButtonPressed = 1;
     }
-    else {
-      rightButtonPressed = 0;
-    }
-  
+  }
+  else {
+    rightButtonPressed = 0;
+  }
+
   return (0);
 }
