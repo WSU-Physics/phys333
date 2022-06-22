5e9e8e2f2b45a8c896600f2f7997fbea2e688d2e
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-22 12:51:03 -0500

    Update lab04partIII.c.ino
    annotated and made sure it looks clean

--- students/severson.spring22/lab04partIII.c/lab04partIII.c.ino
+++ students/severson.spring22/lab04partIII.c/lab04partIII.c.ino
@@ -48,16 +48,16 @@ uint8_t leftSignal(void) {                              //left signal function
   for (l = 3; l < 255; l--) {                           //for loop
     signalPort |= (1 << l);                             //pattern in which the LEDs light up
     _delay_ms(signalDelay);                             //delay
-    if (leftTurn()) {
-      if (leftButtonPressed == 1) {
-        leftButtonPressed = 0;
+    if (leftTurn()) {                                   //call debounce function for left button
+      if (leftButtonPressed == 1) {                     //if state has changed
+        leftButtonPressed = 0;                          //update state
       }
-      else {
-        leftButtonPressed = 1;
+      else {                                            //if state has not changed
+        leftButtonPressed = 1;                          //state remains the same
       }
     }
   }
-  signalPort = 0x00;
+  signalPort = 0x00;                                    //turning all LEDs off before signal runs again
 }
 
 uint8_t rightSignal(void) {                             //right signal function
@@ -65,16 +65,16 @@ uint8_t rightSignal(void) {                             //right signal function
   for (r = 4; r < 8; r++) {                             //for loop
     signalPort |= (1 << r);                             //right signal pattern
     _delay_ms(signalDelay);                             //delay
-    if (rightTurn()) {
-      if (rightButtonPressed == 1) {
-        rightButtonPressed = 0;
+    if (rightTurn()) {                                  //debounce function for right button
+      if (rightButtonPressed == 1) {                    //if state has changed
+        rightButtonPressed = 0;                         //update state
       }
-      else {
-        rightButtonPressed = 1;
+      else {                                            //if state has not changed
+        rightButtonPressed = 1;                         //state remains the same
       }
     }
   }
-  signalPort = 0x00;
+  signalPort = 0x00;                                    //turn all LEDs off before loop runs again
 }
 
 
@@ -89,14 +89,14 @@ int main(void) {                                        //main function
     if (leftTurn()) {                                   //if left button pressed
       if (leftButtonPressed == 0) {                     //but wasn't previously
         leftButtonPressed = 1;                          //update left button state
-        while (leftButtonPressed == 1) {
-          leftSignal();
-          _delay_ms(5 * signalDelay);
+        while (leftButtonPressed == 1) {                //while in this state
+          leftSignal();                                 //call left signal function
+          _delay_ms(5 * signalDelay);                   //delay
         }
       }
     }
     else {                                              //left button not pressed now
-      leftButtonPressed = 0;                            //update state
+      leftButtonPressed = 0;                            //state retains previous value
     }
 
     if (rightTurn()) {                                   //everything is similar, just for the right button & LEDs
