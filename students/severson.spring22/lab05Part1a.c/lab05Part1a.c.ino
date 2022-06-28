0585ad813fc39dd01a86b3b0b36bc2bfef624b21
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-28 17:17:50 -0500

    Update lab05Part1a.c.ino
    got it to receive on pin 10 but still can't get it to give me correct values

--- students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
+++ students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
@@ -6,25 +6,24 @@
 
 #include <SoftwareSerial.h>                   //include serial library
 
-#define sensorPin 10                          //define Rx pin (0 is default on the Arduino Uno R3)
-//#define txPin 1                               //default value (unused)
+#define rxPin 10                          //define Rx pin (0 is default on the Arduino Uno R3)
+#define txPin 1                               //(unused) define Tx pin (1 isdefault valueon the Arduino Uno R3)
 
-uint8_t sensorVal;                            //variable for sensor value
+int sensorVal;                            //variable for sensor value
 
-SoftwareSerial sonarSensor (sensorPin, 1);
+SoftwareSerial sonarSensor (rxPin, txPin);    //define your serial and Rx and Tx pins
 
 void setup() {
-  pinMode(sensorPin, INPUT);                  //set your sensor pin as input
+  pinMode(rxPin, INPUT);                  //set your sensor pin as input
   sonarSensor.begin(9600);                    //begin serial communication, LV-EZ1 uses 9600 baud
   while (!Serial) {                           //wait for the serial port to connect
-    ;
   }
 }
 
 void loop() {
   if (sonarSensor.available() > 0) {
+    delay(96);                                 //LV-EZ1 datasheet states RS232 data can be read every 49 ms and needs to calibrate on startup
     sensorVal = sonarSensor.read();            //read the pin value
     sonarSensor.println(sensorVal, BIN);       //print the value in binary
-    delay(49);                                 //LV-EZ1 datasheet states RS232 data can be read every 49 ms
   }
 }
