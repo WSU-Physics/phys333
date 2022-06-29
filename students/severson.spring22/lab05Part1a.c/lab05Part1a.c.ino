04d1f66c999670580ae9f064343972d428a3b7cb
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-29 12:39:44 -0500

    Update lab05Part1a.c.ino
    trying to  create an array for the reading to go into

--- students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
+++ students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
@@ -12,6 +12,7 @@
 #define txPin 1                               //(unused) define Tx pin (1 is default value on the Arduino Uno R3) (unused)
 
 uint8_t sensorVal;                            //variable for sensor value
+int distance[4];
 
 SoftwareSerial sonarSensor (rxPin, txPin);    //define your serial and Rx and Tx pins
 
@@ -29,8 +30,7 @@ void loop() {
   while(sonarSensor.read() !=82) {
     _delay_us(1);
   }
-    sensorVal = sonarSensor.readBytes(dist, 3);           //read the pin value
+    sensorVal = sonarSensor.read();           //read the pin value
     sonarSensor.println(sensorVal);           //print the sensor value in the serial monitor
                                               //LV-EZ1 datasheet states RS232 data can be read every 49 ms
-  }
 }
