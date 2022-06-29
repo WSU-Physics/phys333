24058eb6da80a82bd979e230658fefafb7b08291
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-29 13:21:48 -0500

    Update lab05Part1a.c.ino
    switched to Serial.

--- students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
+++ students/severson.spring22/lab05Part1a.c/lab05Part1a.c.ino
@@ -11,26 +11,25 @@
 #define rxPin 10                              //define Rx pin (0 is default on the Arduino Uno R3)
 #define txPin 1                               //(unused) define Tx pin (1 is default value on the Arduino Uno R3) (unused)
 
-uint8_t sensorVal;                            //variable for sensor value
+int sensorVal;                            //variable for sensor value
 int distance[4];
 
-SoftwareSerial sonarSensor (rxPin, txPin);    //define your serial and Rx and Tx pins
+//SoftwareSerial Serial (rxPin, txPin);    //define your serial and Rx and Tx pins
 
 void setup() {
   pinMode(rxPin, INPUT);                      //set your sensor pin as input
-  sonarSensor.begin(9600);                    //begin serial communication, LV-EZ1 uses 9600 baud
+  Serial.begin(9600);                    //begin serial communication, LV-EZ1 uses 9600 baud
   while (!Serial) {                           //wait for the serial port to connect
   }
 }
 
 void loop() {
-  while (sonarSensor.available()) {
-    sonarSensor.read();
+  while (Serial.available()) {
+    Serial.read();
   }
-  while(sonarSensor.read() !=82) {
+  while(Serial.read() !=82) {
     _delay_us(1);
   }
-    sensorVal = sonarSensor.read();           //read the pin value
-    sonarSensor.println(sensorVal);           //print the sensor value in the serial monitor
-                                              //LV-EZ1 datasheet states RS232 data can be read every 49 ms
+    sensorVal = Serial.readBytes(distance, 3);           //read the pin value
+    Serial.println(sensorVal);           //print the sensor value in the serial monitor                                           //LV-EZ1 datasheet states RS232 data can be read every 49 ms
 }
