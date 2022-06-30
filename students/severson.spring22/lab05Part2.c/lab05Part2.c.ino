237885c1992216de8170b66f51aa070b5d98a7ff
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-30 15:17:02 -0500

    Update lab05Part2.c.ino
    changed LED definitions. yellow works now, green does not.

--- students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
+++ students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
@@ -6,21 +6,32 @@
 
 #include <SPI.h>
 #include <Wire.h>
-#include <LSM9DS1_Types.h>
 #include <SparkFunLSM9DS1.h>
+#include <LSM9DS1_Types.h>
 #include <LSM9DS1_Registers.h>
+
 
 LSM9DS1 imu;
 
-#define ledG 2
-#define ledY 3
+int ledG = 2;
+int ledY = 3;
 
 void setup() {
   pinMode(ledG, OUTPUT);
   pinMode(ledY, OUTPUT);
-  Wire.begin();
   Serial.begin(115200);
+  Wire.begin();
   
+  if (imu.begin() == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("float-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                   "work for an out of the box LSM9DS1 " \
+                   "Breakout, but may need to be modified " \
+                   "if the board jumpers are.");
+    while (1);
+  }
 }
 
 void loop() {
@@ -28,12 +39,12 @@ void loop() {
     imu.readAccel(); 
   }
   
-  float ax = imu.calcAccel(imu.ax);
-  float ay = imu.calcAccel(imu.ay);
-  float az = imu.calcAccel(imu.az);
+  float aX = imu.calcAccel(imu.ax);
+  float aY = imu.calcAccel(imu.ay);
+  float aZ = imu.calcAccel(imu.az);
   
-  float roll = atan2(imu.ay, imu.az);
-  float pitch = atan2(-imu.ax, sqrt(imu.ay * imu.ay + imu.az * imu.az));
+  float roll = atan2(aY, aZ);
+  float pitch = atan2(-aX, sqrt(aY * aY + aZ * aZ));
 
   float rollDegree = roll *= 180.0 / PI;
   float pitchDegree = pitch *= 180.0 / PI;
@@ -52,5 +63,12 @@ void loop() {
     digitalWrite(ledG, LOW);
     digitalWrite(ledY, LOW);
   }
+
+
+  Serial.println(aX);
+  Serial.println(aY);
+  Serial.println(aZ);
+  Serial.println(rollDegree);
+  Serial.println(pitchDegree);
    
 }
