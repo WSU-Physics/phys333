b1f8702b6779e7263729429bc9a02a63a5bcf3a5
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-30 14:02:23 -0500

    Update lab05Part2.c.ino

--- students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
+++ students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
@@ -1,5 +1,17 @@
 /*
- * Attempt to create a level (tool) using Sparkfun's LSM9DS1 IMU.
- * Light up a yellow LED when within five degrees of level.
+ * Create a level (tool) using Sparkfun's LSM9DS1 IMU.
+ * Light up a yellow LED when between five degrees and one degree of level.
  * Light up a green LED when within one degree of level.
 */
+
+#include <LSM9DS1_Types.h>
+#include <SparkFunLSM9DS1.h>
+#include <LSM9DS1_Registers.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
