d1bcdc1252d1eaf446b5e62320366db1e5141c66
seversonaw <97616537+seversonaw@users.noreply.github.com>
2022-06-30 12:33:28 -0500

    Update lab05Part2.c.ino
    tried changing while to for, still nothing

--- students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
+++ students/severson.spring22/lab05Part2.c/lab05Part2.c.ino
@@ -51,10 +51,10 @@ void loop() {
   float pitch = atan2(-aX, sqrt(aY * aY + aZ * aZ));
   pitch *= 180.0 / PI;
 
-  while (( 1 <= abs(roll) < 5) &&  (1 < abs(pitch) < 5)) {
+  if (( 1 <= abs(roll) < 5) &&  (1 <= abs(pitch) < 5)) {
     digitalWrite(ledYellow, HIGH);
   }
-  while (( 0 <= abs(roll) < 1) && ( 0 <= abs(pitch) < 1)) {
+  if (( 0 <= abs(roll) < 1) && ( 0 <= abs(pitch) < 1)) {
     digitalWrite(ledGreen, HIGH);
   }
   
