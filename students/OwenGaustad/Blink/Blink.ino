5cec89edca18998a011e4286f721dae70a2fe8fd
OwenGaustad2022 <112584622+OwenGaustad2022@users.noreply.github.com>
2024-01-17 20:28:31 -0600

    trying to fix my mistake
    I do not know what this will do

--- students/OwenGaustad/Blink/Blink.ino
+++ students/OwenGaustad/Blink/Blink.ino
@@ -1,12 +1,41 @@
-// This code is broken. See if you can fix it.
+/*
+  Blink
 
-const int LED=9;  // Define LED for pin 9
+  Turns an LED on for one second, then off for one second, repeatedly.
 
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(LED, OUTPUT);  // Set the LED pin as an output
-  digitalWrite(LED, HIGH);  // Set the LED pin high
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(2000);                      // wait for a second
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // Nothing to loop over
-}
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a half second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(250);                       // wait for a quarter second
+} 
