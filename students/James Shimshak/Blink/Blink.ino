7b63a7027d7a97c2c7af934b816508a45dc9507e
J-Shimshak <jshimshak12@gmail.com>
2024-01-24 12:20:39 -0600

    Update Blink.ino

--- students/James Shimshak/Blink/Blink.ino
+++ students/James Shimshak/Blink/Blink.ino
@@ -1,38 +1,26 @@
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);  
+ // pinMode(pin, OUTPUT);
+
+
 }
 
-// the loop function runs over and over again forever
+ // function to do manual pwm
+ //inputs: pin number, how long to stay on, T_on, T_off
+ //outputs: NONE
+ void pwm(uint8_t pin, unsigned int T_on, unsigned int T_off){
+    // turn on LED for specified time
+    digitalWrite(pin, HIGH);
+      delayMicroseconds(T_on);
+    // turn off LED for specified time
+    digitalWrite(pin, LOW);
+    delayMicroseconds(T_off);
+ }
+
 void loop() {
-                    // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  //analogWrite(pin, 127);
+
+  // TODO: Call our manual pwm function
+  pwm(2, 500000, 15000);
 }
