1291932c28b04d12e89a66a5080ce2369fc54278
Adam Beardsley <adam.p.beardsley@gmail.com>
2024-01-24 11:06:31 -0600

    first draft of manual pwm

---
+++ students/beardsley/manual_pwm/manual_pwm.ino
@@ -0,0 +1,26 @@
+int pin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+}
+
+// function to do manual pwm
+// inputs: pin number, Ton, Toff
+// outputs: none
+void pwm(uint8_t pin, unsigned int Ton, unsigned int Toff){
+  // turn on LED for spcified time
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(Ton);
+  // turn off LED for specified time
+  digitalWrite(pin, LOW);
+  delayMicroseconds(Toff);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(pin, 127);
+
+  // TODO: call our manual pwm function
+  pwm(2, 50000, 150000);
+}
