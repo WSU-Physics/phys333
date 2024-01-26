871c856cacf8bee392a28a2046e4c75b4d24f1b5
BeardedJoseph <gentnernj@gmail.com>
2024-01-26 12:38:59 -0600

    Recording V drop

---
+++ students/Gentner/1-1Gentner/1-1Gentner.ino
@@ -0,0 +1,14 @@
+const int BLUE=9; // pin 9 is the blue led
+const int RED=10;// pin 10 is the red led
+const int GREEN=11; // pin 11 is the green led
+const int G
+void setup() {
+pinMode(9,OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT); //set all pins as an output
+}
+
+void loop() {
+  
+
+}
