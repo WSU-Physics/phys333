6e7e5c27a4ed7c2b3a99209aeb8713945dac0af5
Geigus <theGeigus@gmail.com>
2024-01-27 20:05:46 -0600

    Lab 1: Started RGB LED Design Challenge Still need to add rainbow fade LED

---
+++ students/Michael Hrstka/Lab 1/RGB_LED_Design_Challenge/RGB_LED_Design_Challenge.ino
@@ -0,0 +1,109 @@
+int ledR = 3;
+int ledG = 5;
+int ledB = 6;
+int btn = 12;
+int state = 0;
+
+void setup() {
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(btn, INPUT);
+}
+
+void chkBtn() {
+  if (digitalRead(btn) == HIGH) {
+    state = state > 4 ? 0 : state + 1;
+        while (digitalRead(btn) == HIGH){ // Debounce!
+      delay(10);
+    }
+  }
+}
+
+bool ledBlink = true;
+void whiteLED() {
+  digitalWrite(ledR, ledBlink);
+  digitalWrite(ledG, ledBlink);
+  digitalWrite(ledB, ledBlink);
+
+  ledBlink = !ledBlink;
+  delay(100);
+}
+
+int fadePurple;
+bool fadePurpleIn;
+void purpleLED() {
+  analogWrite(ledR, fadePurple);
+  digitalWrite(ledG, LOW);
+  analogWrite(ledB, fadePurple);
+
+  if(fadePurpleIn) {
+      fadePurple++;
+  } else {
+    fadePurple--;
+  }
+
+  if (fadePurple >= 255){
+    fadePurpleIn = false;
+  } else if(fadePurple <= 0){
+    fadePurpleIn = true;
+  }
+
+  delay(3);
+}
+
+int policeFade = 0;
+bool policeFadeIn = true;
+void policeLED() {
+  analogWrite(ledR, 255 - policeFade);
+  digitalWrite(ledG, LOW);
+  analogWrite(ledB, policeFade);
+
+  if(policeFadeIn) {
+      policeFade++;
+  } else {
+    policeFade--;
+  }
+
+  if (policeFade >= 255){
+    policeFadeIn = false;
+  } else if(policeFade <= 0){
+    policeFadeIn = true;
+  }
+
+  delay(1);
+}
+
+
+void gamerLED() {
+
+}
+
+void offLED() {
+  digitalWrite(ledR, LOW);
+  digitalWrite(ledG, LOW);
+  digitalWrite(ledB, LOW);
+}
+
+void loop() {
+  switch(state) {
+  case 0:
+    whiteLED();
+    break;
+  case 1:
+    purpleLED();
+    break;
+  case 2:
+    policeLED();
+    break;
+  case 3:
+    gamerLED();
+    break;
+  default:
+    offLED();
+  }
+  chkBtn();
+  delay(3);
+}
+
+
