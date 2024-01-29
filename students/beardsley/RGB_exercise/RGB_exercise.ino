b3c2befb3028fab82b0cbfeb243b6939453aaf9b
Adam Beardsley <adam.p.beardsley@gmail.com>
2024-01-29 09:35:01 -0600

    RGB modes with button initial commit

---
+++ students/beardsley/RGB_exercise/RGB_exercise.ino
@@ -0,0 +1,40 @@
+const int RLED = 11;  // Red LED pin
+const int GLED = 10;  // Green LED pin
+const int BLED = 9;  // Blue LED pin
+const int BUTTON = 2;  // Button pin
+int state = 0;  // variable for which mode we're in
+
+boolean lastButton = LOW;
+boolean currentButton = HIGH;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RLED, OUTPUT);
+  pinMode(GLED, OUTPUT);
+  pinMode(BLED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+boolean debounce(boolean last){
+  boolean current = digitalRead(BUTTON);
+  if (last != current){
+    delay(5);
+    current = digitalRead(BUTTON);
+  }
+  return current;
+}
+
+void loop() {
+  // Check for button press
+  currentButton = debounce(lastButton);
+  if (lastButton == LOW && currentButton == HIGH){
+    state = (state + 1) % 3;
+  }
+  lastButton = currentButton;
+
+  digitalWrite(RLED, LOW);
+  digitalWrite(GLED, LOW);
+  digitalWrite(BLED, LOW);
+  digitalWrite(state + BLED, HIGH);
+}
