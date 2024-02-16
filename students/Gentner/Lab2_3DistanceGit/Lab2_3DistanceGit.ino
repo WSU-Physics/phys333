fd7f13830228d600e1fc74acdecfc8a94ba92869
BeardedJoseph <gentnernj@gmail.com>
2024-02-16 12:40:39 -0600

    Code is outputting serial after not working while at home, nothing was changed before class

---
+++ students/Gentner/Lab2_3DistanceGit/Lab2_3DistanceGit.ino
@@ -0,0 +1,33 @@
+// v/in 
+// needs a V reading then that will be converted to distance
+// Error response of too close if distance is under 6in 
+// If V=<.25v then print Too Close Back Up
+int Vin;
+int distanceReading;
+int Reading;
+int Sensor = 0; // distance sensor is on pin A0
+unsigned long distance;
+
+void setup() {
+  Serial.begin(9600); //sends info through serial
+  Serial.print("Hello");
+}
+
+void loop() {
+Reading= analogRead(Sensor);
+Vin=map(distanceReading, 0, 1023, 0, 5000); // converts analog into V
+Serial.print("Vin : ");
+Serial.print(Reading );
+
+if(Vin<75)
+{
+  Serial.println(" Too Close");
+}
+else
+{
+distance = Vin * 9.8;
+Serial.println("Distance:");
+Serial.print(distance);
+}
+delay(500);
+}
