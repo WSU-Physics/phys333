b08acd610b9c3e93276e2308c94437d390dc0fbf
lemmonice <112585224+lemmonice@users.noreply.github.com>
2024-02-21 12:29:40 -0600

    Update Blink_jg.ino

--- students/jammes/Blink_jg/Blink_jg.ino
+++ students/jammes/Blink_jg/Blink_jg.ino
@@ -26,12 +26,17 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);  //turn on led
+  delay(1000);                      //for 1 second
+  digitalWrite(LED_BUILTIN, LOW);   //turn off led
+  delay(2000);                      //for TWO seconds
 }
+
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // turn on led
+  delay(500);                       // for half a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn off led
+  delay(250);                       // for a quarter second 25 cents frffrfr
 }
