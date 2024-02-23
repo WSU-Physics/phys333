3ac15f8355b07e8181b4e01b86194af75a41ab5c
tylerkobienia <tyjkobienia@gmail.com>
2024-02-23 12:16:03 -0600

    8LEDTurnOn
    Arduino Code for 8 LEDS.
    blinkAVR is empty.

---
+++ Students/tylerkobienia/8LEDS/8LEDS.ino
@@ -0,0 +1,46 @@
+//const int LED_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9}; //define pin
+
+
+//const int LED_COUNT = 8; // number of LEDS
+
+//void setup() {
+  
+  //for (int i = 0; i < LED_COUNT; i++) { // Set the LED pins as outputs
+    //pinMode(LED_PINS[i], OUTPUT); 
+//  }
+//}
+
+//void loop() {
+//  for (int i = 0; i < LED_COUNT; i++) { // Turn on all the LEDs
+//    digitalWrite(LED_PINS[i], HIGH);
+//  }
+//}
+
+
+// const int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+// const int LED_COUNT = 8;
+
+// const int delayTime = 50;
+
+// void setup() {
+//   // set the led pins as outputs
+//   for (int i = 0; i < LED_COUNT; i++) {
+//     pinMode(ledPins[i], OUTPUT);
+//   }
+// }
+
+// void loop() {
+//   // turn on leds one by one from left to right
+//   for (int i = 0; i < LED_COUNT; i++) {
+//     digitalWrite(ledPins[i], HIGH); 
+//     delay(delayTime); 
+//     digitalWrite(ledPins[i], LOW); 
+//   }
+ 
+//   for (int i = LED_COUNT - 1; i >= 0; i--) {
+//     digitalWrite(ledPins[i], HIGH); 
+//     delay(delayTime); 
+//     digitalWrite(ledPins[i], LOW); 
+//   }
+// }
