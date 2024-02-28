4b7e9c1e2974384561ac51bbb8e79d5fbf219ec7
BeardedJoseph <gentnernj@gmail.com>
2024-02-28 12:18:12 -0600

    Blinks in reverse

---
+++ students/Gentner/4_2Turnsignal/TurnSignal.c
@@ -0,0 +1,41 @@
+// ------- Preamble -------- //
+#include <avr/io.h>                        /* Defines pins, ports, etc */
+#include <util/delay.h>                     /* Functions to waste time */
+
+//Pin Defines
+#define LED_PORT                PORTD
+#define LED_PIN                 PIND
+#define LED_DDR                 DDRD
+#define LED0                    PD0 //R
+#define LED1                    PD1 //G
+#define LED2                    PD2 //B
+#define LED3                    PD3 //W
+#define LED4                    PD4 //W
+#define LED5                    PD5 //B
+#define LED6                    PD6 //G
+#define LED7                    PD7 //R
+//Buttons
+#define BUTTON_PORT             PORTB
+#define BUTTON_PIN              PINB
+#define BUTTON_DDR              DDRB
+#define BUTTON                  PB0
+// Potentially useful macros
+#define BV(bit)               (1 << (bit))  // Mask with single bit set
+#define setBit(byte, bit)     (byte |= BV(bit))  // set bit within byte
+#define clearBit(byte, bit)   (byte &= ~BV(bit)) // clear bit
+#define toggleBit(byte, bit)  (byte ^= BV(bit))  // toggle bit
+
+int main(void){
+DDRD |=Ob11111111;
+
+while(1){
+  PORTD = 0b00000001;
+  delay(250)
+  PORTD = 0b00000010;
+  PORTD = 0b00000100;
+  PORTD = 0b00001000;
+  
+}
+
+
+}
