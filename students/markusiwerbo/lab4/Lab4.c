30305ae89a1abb67640dd17971e4cb1f38ce4c40
iwerbomark <iwerbo@hotmail.com>
2024-03-01 13:22:39 -0600

    Update Lab4.c
    Done with part 2. Working on ways to incorporate buttons

--- students/markusiwerbo/lab4/Lab4.c
+++ students/markusiwerbo/lab4/Lab4.c
@@ -1,11 +1,13 @@
 //Lab 4 
 
 // ------- Preamble -------- //
-#include <avr/io.h>                        /* Defines pins, ports, etc */
-#include <util/delay.h>                     /* Functions to waste time */
-#define DEBOUNCE_TIME 1000
+#include <avr/io.h>                        // Defines pins, ports, etc //
+#include <util/delay.h>                     // Functions to waste time //
+#include <time.h> 
+#define DEBOUNCE_TIME 1000                  // Debounce delay //
+#define DELAY_TIME 300                      // Blink delay // 
 
-// Button Debouncer 
+// Button for left turn Debouncer 
 uint8_t debounce(void) {
   if (bit_is_clear(PINB,PB0)) {
     _delay_us(DEBOUNCE_TIME);
@@ -16,6 +18,29 @@ uint8_t debounce(void) {
   return(0);
 }
 
+uint8_t debounceR(void) {
+  if (bit_is_clear(PINB,PB1)) {
+    _delay_us(DEBOUNCE_TIME);
+    if(bit_is_clear(PINB,PB1)){
+      return(1);
+    }
+  }
+  return(0);
+}
+
+     void shiftRight(){
+      for (int i=4; i<8; i++) {
+        PORTD = (1<<i);
+        _delay_ms(DELAY_TIME);
+      }
+    }
+     void shiftLeft() {
+  for (int i = 3; i>=0; i--) {
+    PORTD = (1<<i);
+    _delay_ms(DELAY_TIME);
+  }
+  
+}
 
 //Part 1 - ToggleButton
 
@@ -23,27 +48,65 @@ int main(void) {
 
   // -------- Inits --------- //
   uint8_t buttonpressed;
+  DDRB &= ~(1<<PB0);          // Button for left turn as input 
   PORTB |= (1<<PB0);
-  DDRD = (1<<PD0); 
-  /*
-  DDRD |= 0b11111111;  
-  DDRB |= 0b11111111;  
-  */
+  DDRB &= ~(1<<PB1);          // Button for right turn as input 
+  PORTB |= (1<<PB1);
+  DDRD |= 0b11111111;         // PORTD as output for LEDs
+  int buttonLeft = 0;             // Interger for button toggle 
+  int buttonRight = 0;
+
+time_t start,end;
+double dif;
+
+time (&start);
+  
+
 
   // ------ Event loop ------ //
   while (1) {
     if (debounce()){
     if (bit_is_clear(PINB, PB0)) {
       if (buttonpressed==0) {
-        PORTD ^= (1<<PD0);
+        buttonLeft = !buttonLeft;
+
+    
+
+        
         buttonpressed=1;
       }
     }
     }
   
+      if (debounceR()){
+    if (bit_is_clear(PINB, PB1)) {
+      if (buttonpressed==0) {
+        buttonRight = !buttonRight;
+
+    
+
+        
+        buttonpressed=1;
+      }
+    }
+    }
+
     else {
    buttonpressed=0;
     }
+    if(buttonLeft) {
+     // time (&end);
+//dif = difftime (end,start);
+//if (dif > 300) {
+
+      shiftLeft();
+      //time (&start);
+//}
+    }
+    if (buttonRight) {
+      shiftRight();
+    }
   }
+
     return(0);
 }
