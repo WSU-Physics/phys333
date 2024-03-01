bb0acf52b68cf4ca89cbf82b1c04557496ab27b1
Geigus <theGeigus@gmail.com>
2024-03-01 12:31:33 -0600

    Lab 4 - V2: Turn signals now work with button presses, no break lights yet. Signals do not currently finish pattern when released, will fix.

--- students/Michael Hrstka/Lab 4/turn_signal/turn_signal.c
+++ students/Michael Hrstka/Lab 4/turn_signal/turn_signal.c
@@ -1,46 +1,77 @@
+#include <avr/sfr_defs.h>
 // ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define setLed(byte) PORTD = byte;
+
 int ledLPos = 0;
 int ledRPos = 0;
-int signalActive = 0; // This should eventually be just 3 states: OFF, RIGHT and LEFT (BREAK will still be seperate)
-_Bool isRight = 0; // 0: Left, 1: Right
+int bitOff = 0;
+int tickSpeed = 100;
+_Bool rightPressed = 0;
+_Bool leftPressed = 0;
+_Bool breakPressed = 0;
+int leftPin = 0;
+int rightPin = 2;
+int breakPin = 1;
 
-void flashSignal() {
-  if(isRight){
-    // PORTD = 0b00000000; // Right side off?
-    PORTD = (1 << 3-ledRPos); // Set right
+void flashRight() {
+  setLed(1 << 3-ledRPos); // Set right
     
     ledLPos = 0;
-    ledRPos = ledRPos < 3 ? ledRPos + 1 : 0;
-  } else {
-    // PORTD = 0b00000000; // Left side off
-    PORTD = (1 << ledLPos) << 4; // Set left
+
+    if(ledRPos < maxLed) {
+      ledRPos++;
+    } else {
+      ledRPos = 0;
+    }
+}
+
+void flashLeft() {
+  setLed((1 << ledLPos) << 4); // Set left
     ledRPos = 0;
-    ledLPos = ledLPos < 3 ? ledLPos + 1 : 0;
+    if(ledLPos < maxLed) {
+      ledLPos++;
+    } else {
+      ledLPos = 0;
+    }
+}
+
+void flashSignal() {
+
+  if(rightPressed){
+    flashRight();
+  } else if(leftPressed) {
+    flashLeft();
+  } else {
+    ledRPos = 0;
+    ledLPos = 0;
+    setLed(0x00);
   }
 }
 
-int main(void){
-  // Declare variables
-  uint8_t bita; // Do I need these?
-  uint8_t bitb;
+void checkBtns(){
+  leftPressed = bit_is_clear(PINC, leftPin);
+  rightPressed = bit_is_clear(PINC, rightPin);
+  breakPressed = bit_is_clear(PINC, breakPin);
+  // NOTE: Pass by ref -> fun(int& i); --> call with: fun(&i);
 
+}
+
+int main(void){
   // Set up LEDs
   DDRD = 0xff;  // All output
-  int stateCnt = 0;
+  DDRC = 0x00; // All inputs (cause I'm lazy);
+  PORTC = 0xFF; // Pullup
 
   while(1) {
-    if(stateCnt > 15){
-      stateCnt = 0;
-      isRight = !isRight;
-    } else {
-      stateCnt++;
-    }
+
+    _delay_ms(tickSpeed); // Blatently stolen from Dr. Beardsley
+
+    checkBtns();
 
     flashSignal();
-    _delay_ms(100);
   }
   
   return (0);
