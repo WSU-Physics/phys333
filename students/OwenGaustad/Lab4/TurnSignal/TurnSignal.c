75f410ce845cc41b9c9692b050256623793708f4
OwenGaustad2022 <112584622+OwenGaustad2022@users.noreply.github.com>
2024-03-13 15:33:40 -0500

    This is the final code for Lab4
    I still have to upload my other version but this version is the one I like the most

--- students/OwenGaustad/Lab4/TurnSignal/TurnSignal.c
+++ students/OwenGaustad/Lab4/TurnSignal/TurnSignal.c
@@ -2,63 +2,73 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define LED_PORT                PORTD
+#define LED_DDR                 DDRD
+
 #define BUTTON_PORT             PORTB
 #define BUTTON_PIN              PINB
 #define BUTTON_DDR              DDRB
 
 #define BUTTON                  PB0
 #define BUTTON2                 PB1
-#define DELAY  5                            /* microseconds */
 
-// Potentially useful macros
-#define BV(bit)               (1 << (bit))  // Mask with single bit set
-#define setBit(byte, bit)     (byte |= BV(bit))  // set bit within byte
-#define clearBit(byte, bit)   (byte &= ~BV(bit)) // clear bit
+#define BV(bit)               (1 << (bit))       // Mask with single bit set
 #define toggleBit(byte, bit)  (byte ^= BV(bit))  // toggle bit
 
-int activeState = 0;
+int offState = 0;  //This variable goes between 0 and 1 and controls when all lights turn off
+int delay;         //This is used in my for loops and is used as the integer increaded to our maximum time
+int led;           //This variable is used to determine which LED 0-7 is turned on
+int time;          //This variable is used with the method variableDelay() and is our value for waited time in milliseconds
+/***********************************************************************************************
 
-/*****************************************************************************************/
+This method activates the state for my leds to shift from pin 3 to 7 first it initializes
+
+the third pin and changes its state which turns it off because I have it initiallly set to
+
+on this is unnoticable and it alowed me to have all the LED's turn on/off not the first one
+
+constantly be on I could fix this without turning the 3rd LED on but that would involve
+
+another delay and I don't want that. This method also calls the varriableDelay() twice the
+
+first delay is roughly 100ms the second takes place after they are all turned on and is 200ms
+***********************************************************************************************/
 leftTurn()  {
-  PORTD = 0b00001000;
-  for(int i = 3; i <= 8; i++) {
-    toggleBit(PORTD, i);
-    for(int factorDelay = 0; factorDelay <= 99; factorDelay++) {
-      _delay_ms(1);
-      if (leftBtn() == 1 | rightBtn()==1){
-        break;
-      }
-    }
+  LED_PORT = 0b00001000;
+  for(led = 3; led <= 7; led++) {
+    toggleBit(LED_PORT, led);
+    variableDelay(100);
   }
-  for(int factorDelay = 0; factorDelay <= 99; factorDelay++) {
-    _delay_ms(2);
-    if (leftBtn() == 1 | rightBtn()==1){
-      break;
-    }
-  }
+  variableDelay(200);
 }
+/***********************************************************************************************
 
-/*****************************************************************************************/
+This method activates the state for my leds to shift from pin 4 to 0 first it initializes
+
+the fourth pin and changes its state which turns it off because I have it initiallly set to
+
+on this is unnoticable and it alowed me to have all the LED's turn on/off not the first one
+
+constantly be on I could fix this without turning the 4th LED on but that would involve
+
+another delay and I don't want that. This method also calls the varriableDelay() twice the
+
+first delay is roughly 100ms the second takes place after they are all turned on and is 200ms
+***********************************************************************************************/
 rightTurn() {
-  PORTD = 0b00010000;
-  for(int i = 4; i >= 0; i--) {
-    toggleBit(PORTD, i);
-    for(int factorDelay = 0; factorDelay <= 99; factorDelay++) {
-      _delay_ms(1);
-      if (leftBtn() == 1 | rightBtn() == 1){
-        break;
-      }
-    }
+  LED_PORT = 0b00010000;
+  for(led = 4; led >= 0; led--) {
+    toggleBit(LED_PORT, led);
+    variableDelay(100);
   }
-  for(int factorDelay = 0; factorDelay <= 99; factorDelay++) {
-    _delay_ms(2);
-    if (leftBtn() == 1 | rightBtn() == 1){
-      break;
-    }
-  }
+  variableDelay(200);
 }
+/***********************************************
 
-/*****************************************************************************************/
+This method checks to see if the left button
+
+was clicked then returns 1 if yes and 0 if no
+***********************************************/
 leftBtn() {
   if(bit_is_clear(BUTTON_PIN, BUTTON2)){
     return 1;
@@ -67,8 +77,12 @@ leftBtn() {
     return 0;  
   }
 }
+/***********************************************
 
-/*****************************************************************************************/
+This method checks to see if the right button 
+
+was clicked then returns 1 if yes and 0 if no 
+***********************************************/
 rightBtn() {
   if(bit_is_clear(BUTTON_PIN, BUTTON)){
     return 1;
@@ -77,8 +91,23 @@ rightBtn() {
     return 0;  
   }
 }
+/*******************************************************************************
 
-/*****************************************************************************************/
+This method takes an integer value and stores it as a temporary variable 
+
+time Then the delay lasts for the input delay time in milliseconds              
+*******************************************************************************/
+variableDelay(time) {
+  for (delay = 0; delay <= time; delay++) {
+    _delay_ms(1);
+    if (leftBtn() == 1 | rightBtn() == 1){
+      break;
+    }
+  }
+}
+/**************
+* Main Method *
+**************/
 int main(void){
   uint8_t bita;                       //
   uint8_t bitb;                       //
@@ -86,52 +115,69 @@ int main(void){
   BUTTON_PORT |= (1 << BUTTON);       //
   BUTTON_DDR &= ~(1 << BUTTON2);      //
   BUTTON_PORT |= (1 << BUTTON2);      //
-
-
-  // Set up LEDs
-  DDRD = 0xff;  // All output
+  LED_DDR = 0xff;                     // All output
 
   /****************************
-  *This Loops the LED states  *
+  * This Loops the LED states *
   ****************************/
   while (1){
     /**********************************
     * This Statement sets the LED     *
     * state to all pins off           *
     **********************************/
-    if (activeState == 1) {
-      while(rightBtn()==0 && leftBtn()==0 ){
-        PORTD = 0b00000000;
+    if (offState == 1) {
+      while (rightBtn() == 1 || leftBtn() == 1){
+        _delay_us(5);
       }
-      activeState = 0;
+      while(rightBtn()==0 && leftBtn()==0 ){
+        LED_PORT = 0b00000000;
+      }
+      offState = 0;
     }
-    /**********************************
-    * This Statement sets the LED     *
-    * state to the right turn method  *
-    **********************************/
+    /************************************
+    * This Statement sets the LED       *
+    * state to the right turn method    *
+    * if the right button is hit a      *
+    * second time the loop ends and     *
+    * all LED's are turned off but if   *
+    * the left button is hit it         *
+    * merely activates the other lights.* 
+    ************************************/
     else if (rightBtn())  {
       while(rightBtn()==1){
-        _delay_us(DELAY);
+        _delay_us(5);
       }
-
       while(rightBtn()==0 && leftBtn()==0 ){
         rightTurn();
-        activeState = 1;
+      }
+      if (rightBtn() == 1) {
+        offState = 1;
+      }
+      if (leftBtn() == 1) {
+        offState = 0;
       }  
     }
-
-    /**********************************
-    * This Statement sets the LED     *
-    * state to the left turn method   *
-    **********************************/
+    /************************************
+    * This Statement sets the LED       *
+    * state to the left turn method     *
+    * if the left button is hit a       *
+    * second time the loop ends and     *
+    * all LED's are turned off but if   *
+    * the right button is hit it        *
+    * merely activates the other lights.* 
+    ************************************/
     else if (leftBtn())  {
       while(leftBtn() == 1){
-        _delay_us(DELAY);
+        _delay_us(5);
       }
-
       while(rightBtn()==0 && leftBtn()==0 ){
         leftTurn();
-        activeState = 1;
+      }
+      if (rightBtn() == 1) {
+        offState = 0;
+      }
+      if (leftBtn() == 1) {
+        offState = 1;
       }
     }
   }
