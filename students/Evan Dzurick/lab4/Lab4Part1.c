c629a49127b19ccd1081a7d7f4414e49b3f9f458
EvanDzurick <evan.dzurick@go.winona.edu>
2024-03-15 22:10:32 -0500

    Lab 4 Part 3

--- students/Evan Dzurick/lab4/Lab4Part1.c
+++ students/Evan Dzurick/lab4/Lab4Part1.c
@@ -1,59 +1,90 @@
-// ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define DEBOUNCE_TIME 1000 /* microseconds */
-#define LED_PORT                PORTB
-#define LED_PIN                 PINB
-#define LED_DDR                 DDRB
+#define LED_PORT        PORTB
+#define LED_DDR         DDRB
+#define LED1            PB0
+#define LED2            PB1
+#define LED3            PB2
+#define LED4            PB3
+#define LED5            PD4
+#define LED6            PD5
+#define LED7            PD6
+#define LED8            PD7
 
-#define LED0                    PB0
-#define LED1                    PB1
-#define LED2                    PB2
-#define LED3                    PB3
-#define LED4                    PB4
-#define LED5                    PB5
-#define LED6                    PB6
-#define LED7                    PB7
+#define BUTTON_PORT     PORTD
+#define BUTTON_PIN      PIND
+#define BUTTON_DDR      DDRD
+#define BUTTON1         PD2
+#define BUTTON2         PD3
 
-#define BUTTON_PORT             PORTD
-#define BUTTON_PIN              PIND
-#define BUTTON_DDR              DDRD
+#define BUTTON_HOLD_DELAY    50
 
-#define BUTTON                  PD2
+void init() {
+    // Set LED pins as output
+    LED_DDR |= (1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4);
+    DDRD |= (1 << LED5) | (1 << LED6) | (1 << LED7) | (1 << LED8); 
 
-
-uint8_t debounce(void) {
- if (bit_is_clear(BUTTON_PIN, BUTTON)) { /* button is pressed now */
- _delay_us(DEBOUNCE_TIME);
- if (bit_is_clear(BUTTON_PIN, BUTTON)) { /* still pressed */
- return (1);
- }
- }
- return (0);
+    
+    BUTTON_DDR &= ~((1 << BUTTON1) | (1 << BUTTON2));
+    BUTTON_PORT |= (1 << BUTTON1) | (1 << BUTTON2);
 }
+
 int main(void) {
- // -------- Inits --------- //
- uint8_t buttonWasPressed; /* state */
- BUTTON_PORT |= (1 << BUTTON); /* enable the pullup on the button */
+    init();
+    uint8_t currentLED = 0;
+    uint8_t currentLED2 = 4;
 
- LED_DDR = (1 << LED0); /* set up LED for output */
- LED_DDR = (1 << LED2); /* set up LED for output */
+    while (1) {
+        if ((BUTTON_PIN & (1 << BUTTON1))) { // Button1 is held
+            // Sequentially turn on 4 different LEDs
+            for (int i = 0; i < 4; i++) {
+                // Turn off the previous LED
+                LED_PORT &= ~(1 << currentLED);
 
- // ------ Event loop ------ //
- while (1) {
- if (debounce()) { /* debounced button press */
- if (buttonWasPressed == 0) { /* but wasn't last time through */
+                // Move to the next LED (cycling from LED0 to LED3)
+                currentLED = (currentLED + 1) % 4;
 
- LED_PORT ^= (1 << LED0); /* do whatever */
- LED_PORT ^= (1 << LED2); /* do whatever */
+                // Turn on the current LED
+                LED_PORT |= (1 << currentLED);
 
- buttonWasPressed = 1; /* update the state */
- }
- }
- else { /* button is not pressed now */
- buttonWasPressed = 0; /* update the state */
- }
- } /* End event loop */
- return (0); /* This line is never reached */
+                // Delay for some time 
+                _delay_ms(200);
+            }
+        } else {
+            // Turn off all LEDs 
+            LED_PORT &= ~((1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4));
+        }
+
+        if ((BUTTON_PIN & (1 << BUTTON2))) {
+          
+          
+            for (int i = 0; i < 4; i++) {
+                // Turn off the previous LED
+                PORTD &= ~(1 << currentLED2);
+
+                // Move to the next LED (cycling from LED5 to LED8)
+                currentLED2 = (currentLED2 - PD4 + 1) % 4 + PD4;
+
+                // Turn on the current LED
+                PORTD |= (1 << currentLED2);
+
+                // Delay for some time 
+                _delay_ms(200);
+            }
+        } else {
+            // Turn off all LEDs 
+            PORTD &= ~((1 << LED5) | (1 << LED6) | (1 << LED7) | (1 << LED8));
+        }
+
+
+
+
+
+      
+
+        _delay_ms(BUTTON_HOLD_DELAY);
+    }
+
+    return 0;
 }
