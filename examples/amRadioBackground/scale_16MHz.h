f6a1dba43fe6af11ec8b16d237345989613cf4e3
Adam Beardsley <adam.p.beardsley@gmail.com>
2024-04-19 12:04:23 -0500

    Starting work on am radio in the background

---
+++ examples/amRadioBackground/scale_16MHz.h
@@ -0,0 +1,29 @@
+/* Assumes 16 MHz clock and 128 prescalar */
+/* value = 16 MHz / (2 * 128 * f) */
+
+#define REST 0
+#define C4 239
+#define Cx4 226
+#define D4 213
+#define Dx4 201
+#define E4 190
+#define F4 179
+#define Fx4 169
+#define G4 159
+#define Gx4 150
+#define A4 142
+#define Ax4 134
+#define B4 127
+#define C5 119
+#define Cx5 113
+#define D5 106
+#define Dx5 100
+#define E5 95
+#define F5 89
+#define Fx5 84
+#define G5 80
+#define Gx5 75
+#define A5 71
+#define Ax5 67
+#define B5 63
+#define C6 60
