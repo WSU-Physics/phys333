a6cfa078e2ace4cc58e229b21709dc8a574a952d
OwenGaustad2022 <112584622+OwenGaustad2022@users.noreply.github.com>
2024-04-20 22:05:26 -0500

    Comments
    I finished all comments and code for everything that doesn't involve the motor. I am going to attempt to run this code now and start working on the errors I find. I am already thinking I know what one error will be and it will involve the button press.

--- students/OwenGaustad/FinalProjectCode/ButtonLockWithLEDS/ButtonLockWithLEDS.c
+++ students/OwenGaustad/FinalProjectCode/ButtonLockWithLEDS/ButtonLockWithLEDS.c
@@ -29,21 +29,18 @@
 #define MOTOR                   PB5     //Digital Pin 13
 
 // ------- Macros -------- //
-
 #define BV(bit)               (1 << (bit))       // Mask with single bit set
 #define setBit(byte, bit)     (byte |= BV(bit))  // set bit within byte
 #define clearBit(byte, bit)   (byte &= ~BV(bit)) // clear bit
 #define toggleBit(byte, bit)  (byte ^= BV(bit))  // toggle bit
 
 // ------- Global Variables -------- //
-
-const int setPasssword = [1,2,3,4];              //Sets the password you need for checking if the buttons were clicked in the correct order
-const int delay = 5;                             //Sets the delay that is used for a small delay in the button press. 
-int getPassword[3];                              //This establishes the array we will be using for our input password. 
-int getIndex = 0;                                //This establishes a variable I use for shifting the index of my get password array
+const int setPasssword = [1,2,3,4]; //Sets the password you need for checking if the buttons were clicked in the correct order
+const int delay = 5;                //Sets the delay that is used for a small delay in the button press. 
+int getPassword[3];                 //This establishes the array we will be using for our input password. 
+int getIndex = 0;                   //This establishes a variable I use for shifting the index of my get password array
 
 // ------- Functions -------- //
-
 /******************************************************************************
 This Function checks the button pin that is being recieved and if that button 
 is pressed it waits 5 milliseconds then if it is still depressed it returns 
@@ -83,7 +80,7 @@ how we will be able to keep our pasword to 4 digits. There is also a clear
 function which will be button 7 and if that is hit the index will be reset 
 to 0 and our LED's will all be reset. 
 ******************************************************************************/
-updatePassword()  {
+void updatePassword()  {
   getPassword[getIndex] = chkButton();
   if (getPassword[getIndex] == 6) {
     getIndex = 0;
@@ -110,6 +107,7 @@ void intInterrupt0()  {
     PCMSK2 |= (1 << i);} //sets up a loop that initializes my interrupts
   sei();
 }
+
 /******************************************************************************
 Function activates interrupts our arduino and runs our updatePassword 
 function.
@@ -117,7 +115,10 @@ function.
 ISR(PCINT2_vect) {
   updatePassword();
 }
+
 /******************************************************************************
+This function turns all LEDs off. Although the motor is on the same 
+directory as the LEDs this function does not effect the motor pin.
 ******************************************************************************/
 void ledsOff(){
   //turns all leds to off
@@ -125,7 +126,10 @@ void ledsOff(){
     clearBit(LED_PORT,i); 
   }
 }
+
 /******************************************************************************
+This function turns one LED on. Although the motor is on the same 
+directory as the LEDs this function does not effect the motor pin.
 ******************************************************************************/
 void oneLED(){
   //turns on first led
@@ -133,18 +137,29 @@ void oneLED(){
 }
 
 /******************************************************************************
+This function turns two LEDs on. Although the motor is on the same 
+directory as the LEDs this function does not effect the motor pin.
 ******************************************************************************/
 void twoLED(){
   //turns on first 2 leds
   setBit(LED_MOTOR_PORT, GREEN_LED2);
 }
+
 /******************************************************************************
+This function turns three LEDs on. Although the motor is on the same 
+directory as the LEDs this function does not effect the motor pin.
 ******************************************************************************/
 void threeLED(){
   //turns on first 3 leds
   setBit(LED_MOTOR_PORT, GREEN_LED3);
 }
+
 /******************************************************************************
+This function turns four LEDs on. Although the motor is on the same 
+directory as the LEDs this function does not effect the motor pin. This 
+function is also structured differently than the other LED states because 
+it takes place in a state where no user input is allowed so it needs a 
+delay followed by the turning off of all LEDs. 
 ******************************************************************************/
 void fourLED(){
   setBit(LED_MOTOR_PORT, GREEN_LED4);
@@ -152,32 +167,63 @@ void fourLED(){
   ledsOff();
   _delay_ms(100);
 }
+
 /******************************************************************************
+This function takes place when all LEDs should already be off then it uses 
+two for loops to initialize each green LED pin as either on or off and 
+toggle them through the collective states on, off, on, off. it waits 200ms 
+between each state and ends the state with all of them off. In the code we 
+set the states of all LEDs to off before and after this funtion runs so it 
+might seem like redundant code but if I decide to make changes in the 
+future I might need them. Plus it doesn't change anything having them in 
+there as a checker. 
 ******************************************************************************/
 void passwordCorrect(){
+  ledsOff();
   for(int i = 0; i<=3; i++){
     for(int i = 1; i <= 4;i++){
       toggleBit(LED_MOTOR_PORT, i);
     }
     _delay_ms(200);
   }
+  ledsOff();
   //turn motor forward
   //turn motor back to  start position(theoretccially this won't lock it)
 }
+
 /******************************************************************************
+This function takes place when all LEDs should already be off then it 
+flashes the red LEDs on, off, on, off. it waits 200ms between each state 
+and ends the state with all of them off. In the code we set the states of 
+all LEDs to off before and after this funtion runs so it might seem like 
+redundant code but if I decide to make changes in the future I might need 
+them. Plus it doesn't change anything having them in there as a checker. 
 ******************************************************************************/
 void passwordIncorrect(){
+  ledsOff();
   for(int i = 0; i<=3; i++){
     toggleBit(LED_MOTOR_PORT, ALL_RED_LEDS);
     _delay_ms(200);
   }
+  ledsOff();
 }
+
 /******************************************************************************
 ******************************************************************************/
 void motorAction(){
   //turn motor set distance in one direction and then that same distance back
 }
+
 /******************************************************************************
+This function checks to see if the flag integer is equal to one. the only 
+time the flag variable should be equal to one is after your password has 
+been complete. Then all the buttons are immediately deactivated. Following 
+the deactivation the flag is set to zero, the fourth button lights up for a 
+second, turns off for .1 second, then finally our password checker is ran. 
+If the input password is equal to our set password then the correctPassword 
+function is run. Otherwise the incorrect Function is run. At the end of the 
+Flag check the interrupts are reinitialized with sei() allowing you to 
+reinput a password. 
 ******************************************************************************/
 void flagCheck(){
   if (flag == 1) {
@@ -194,26 +240,34 @@ void flagCheck(){
   }
 }
 
-
+// ------- Main Code -------- //
 int main(void) {
-// ------- Inits -------- //
 
-  int flag = 0;     //initializes flag that will be used to turn off our buttons after the password has been input
-  intInterrupt0();  //initializes our interrupts
+  // ------- Inits -------- //
+  //Initializes flag that will be used to turn off our buttons after the password has been input
+  int flag = 0;
+  //Initializes our interrupts
+  intInterrupt0();
+  //Initializes the directory for our LED's and motor
   LED_MOTOR_DDR = 0xff;
   
-  // ------- Code -------- //
+  // ------- Loop -------- //
   while (1){
-    switch(getIndex){
-      case(0):
-        flagCheck();
-        ledsOff();
+    //an array that indexes from 0 to 3
+    switch(getIndex){   
+      //this state is run when a button has not been pressed or our password has been fully input. 
+      case(0):          
+      flagCheck();
+      ledsOff();
+      //This state is run after one number has been input. 
       case(1):
-        oneLED();
+      oneLED();
+      //This state is run after two numbers have been input. 
       case(2):
-        twoLED();
+      twoLED();
+      //This state is run after three numbers have been input. 
       case(3):
-        threeLED();
+      threeLED();
     }
   }  
 }
