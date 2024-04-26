59d5629c6312ce47daafee789605938809dd1911
BeardedJoseph <gentnernj@gmail.com>
2024-04-26 12:51:39 -0500

    Reads 414 only

--- students/Gentner/Phys333_Finalproj/Phys333_Finalproj.ino
+++ students/Gentner/Phys333_Finalproj/Phys333_Finalproj.ino
@@ -18,6 +18,9 @@ const int Lights=13;
 const int PR=11;
 const int IR=4;
 const int IRD=A0;
+boolean lastDetect = LOW; // variable containing the previous Detect state
+boolean currentDetect = LOW; // variable constaing the current Detect state
+unsigned int ledMode = 0; // present state of the Blue Light
 
 
 void setup() {
@@ -26,24 +29,50 @@ pinMode(IRD,INPUT);
 Serial.begin(9600);
 }
 
+boolean debounce(boolean last)
+{
+ boolean current = digitalRead(IRD); // Read the Detect state
+ if (1020 >= current) // if it's less than ambient
+ {
+ delay(3); //Wait 5ms
+ currentDetect = digitalRead(IRD); //Read it again
+ }
+ return current; //Return the current value
+}
+
+
+
 void loop() {
+  //Detector Debounce
+  {
+    currentDetect = debounce(lastDetect); //Read debounced state
+ if (lastDetect == LOW &&currentDetect == HIGH) //if it was pressed...
+ {
+ ledMode++  ; //Increase the LED state value
+ }
+ lastDetect =currentDetect; //Reset Detect value
+ if (ledMode == 2) ledMode=1;
+ switch(ledMode);
   // put your main code here, to run repeatedly:
+  }
 int DetectRead = 0;
 int Detect = 0;
 digitalWrite(IR, HIGH);
 DetectRead = analogRead(IRD);
 Detect=map(DetectRead, 0, 1023, 0, 5000);
-Serial.print("High ");
-Serial.print(DetectRead);
-Serial.print(" - ");
-Serial.println(Detect);
-delay(500);
-digitalWrite(IR,LOW);
-DetectRead = analogRead(IRD);
-Detect=map(DetectRead, 0, 1023, 0, 5000);
-Serial.print("Low ");
-Serial.print(DetectRead);
-Serial.print(" - ");
-Serial.println(Detect);
-delay(500);
+Serial.println(DetectRead);
+delay(100);
+//Serial.print("High ");
+
+//Serial.print(" - ");
+//Serial.println(Detect);
+//delay(500);
+//digitalWrite(IR,LOW);
+//DetectRead = analogRead(IRD);
+//Detect=map(DetectRead, 0, 1023, 0, 5000);
+//Serial.print("Low ");
+//Serial.print(DetectRead);
+//Serial.print(" - ");
+//Serial.println(Detect);
+//delay(500);
 }
