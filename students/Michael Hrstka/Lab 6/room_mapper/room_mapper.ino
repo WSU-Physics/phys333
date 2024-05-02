1c9c5a5a358af6f895940ed6e0a5b1a0e9b9ffc0
Geigus <theGeigus@gmail.com>
2024-05-02 13:24:15 -0500

    Not functional but the concept is there at least... Not sure why it's broken, things just all stopped working.

--- students/Michael Hrstka/Lab 6/room_mapper/room_mapper.ino
+++ students/Michael Hrstka/Lab 6/room_mapper/room_mapper.ino
@@ -1,15 +1,21 @@
 #include <SD.h>
+#include <avr/io.h>
+#include <util/delay.h>
 #include <Wire.h>
-#include "distance_sensor.h" // Hopefully this works
+#include "USART.h"
+#include <util/setbaud.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
 
 const int sdPin = 10;
-int distance;
-int angle;
+int distancePin = 0;
 
 char serialCharacter;
-DistanceSensor distanceSensor = DistanceSensor::DistanceSensor();
+// DistanceSensor distanceSensor = DistanceSensor::DistanceSensor();
 
 File logfile;
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 void logToSD(int distance, double angle){
   Serial.print(distance);
@@ -25,7 +31,12 @@ void logToSD(int distance, double angle){
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+  }
+  Serial.println("LIS3DH found!");
 
   // SD Output
   pinMode(sdPin, OUTPUT);
@@ -50,11 +61,39 @@ void setup() {
 
   logfile.print("Distance, Angle,");
   logfile.print("\n");
+
+  // initUSART();
+}
+
+double getDistance() {
+  double distance = analogRead(distancePin) * 5.0 / 1023;
+  Serial.println(distance); 
+  return distance;
+}
+
+double getLevel() {
+  sensors_event_t event;
+  lis.getEvent(&event);
+  double z = event.acceleration.z;
+  return 180.0 - 180.0*(z/9.8);
 }
 
 void loop() {
-  delay(500);
-  distance = distanceSensor.getDistance();
-  const 
+  delay(200);
+  double distance = getDistance();
+  double angle = getLevel();
+  Serial.println(angle);
   logToSD(distance, angle);
 }
+
+// void initUSART(void) { 
+//   UBRR0H = UBRRH_VALUE;
+//   UBRR0L = UBRRL_VALUE;
+// #if USE_2X
+//   UCSR0A |= (1 << U2X0);
+// #else
+//   UCSR0A &= ~(1 << U2X0);
+// #endif
+//   UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+//   UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+// }
