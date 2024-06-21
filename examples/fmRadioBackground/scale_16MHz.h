61cfa485fafc8b8e9c013ec3a609869a3f78ffb9
Adam Beardsley <adam.p.beardsley@gmail.com>
2024-06-21 09:09:47 -0500

    start FM transmit. update periods

---
+++ examples/fmRadioBackground/scale_16MHz.h
@@ -0,0 +1,30 @@
+/* Assumes 16 MHz clock and 8 prescalar */
+/* For FM we are interrupting every 1/4 period
+/* value = 16 MHz / (4 * 8 * f) */
+
+#define REST 0
+#define C4 1911
+#define Cx4 1804
+#define D4 1703
+#define Dx4 1607
+#define E4 1517
+#define F4 1432
+#define Fx4 1351
+#define G4 1276
+#define Gx4 1204
+#define A4 1136
+#define Ax4 1073
+#define B4 1012
+#define C5 956
+#define Cx5 902
+#define D5 851
+#define Dx5 804
+#define E5 758
+#define F5 716
+#define Fx5 676
+#define G5 638
+#define Gx5 602
+#define A5 568
+#define Ax5 536
+#define B5 506
+#define C6 478
