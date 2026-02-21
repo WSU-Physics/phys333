fa997ade3ff90d77b870f69ae055658fdbd02c94
Adam Beardsley <adam.p.beardsley@gmail.com>
2026-02-21 12:50:07 -0600

    Revert "parts 1-3 on Lab 1"
    This reverts commit 676e018138b30300e0d12b7b0378ca26e0342c05.

--- exercises/BlinkWithoutDelayFillInBlanks/BlinkWithoutDelayFillInBlanks.ino
+++ exercises/BlinkWithoutDelayFillInBlanks/BlinkWithoutDelayFillInBlanks.ino
@@ -29,7 +29,7 @@
 */
 
 // constants won't change. Used here to set a pin number:
-const int ledPin = LED_BUILTIN;// the number of the LED pin
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
@@ -43,7 +43,7 @@ const long interval = 1000;           // interval at which to blink (millisecond
 
 void setup() {
   // set the digital pin as output:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(XXX, YYY);
 }
 
 void loop() {
@@ -54,16 +54,14 @@ void loop() {
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+  if (ZZZ) {
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
+    previousMillis = XXX;
 
     // if the LED is off turn it on and vice-versa:
-    ledState = !ledState; 
-  }
-    // set the LED with the ledState of the variable:
-    digitalWrite(LED_BUILTIN, ledState);
-  
-}
+    [...] // Fill in - relevant variable is ledState
 
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+}
