7024884f84837948599e1367afcd98368ca7129d
lexi-freiboth <alexisfreiboth@gmail.com>
2026-02-27 13:52:56 -0600

    Lab 2 updates 2/27

--- students/Lexi/Lab2/part2_lab2/part2_lab2.ino
+++ students/Lexi/Lab2/part2_lab2/part2_lab2.ino
@@ -1,36 +1,34 @@
 
 const uint8_t PWM_pin = 9; 
 
-const unsigned long PWM_period_us = 10000; 
+const unsigned long PWM_period_us = 1000; 
 const unsigned long SAW_period_ms = 2000; 
-const int steps = 1000; 
-const unsigned long Step_time_ms = SAW_period_ms / steps; 
+const int steps = 100; 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(PWM_pin, OUTPUT); 
 }
 
-void pwmFixed (int duty) {
-  unsigned long t0 = micros(); 
-
+void pwmSoft(uint8_t pin, uint8_t duty, unsigned long duration_ms) {
   unsigned long ton = (PWM_period_us * duty) / steps; 
+  unsigned long startTime = millis(); 
 
-  if(ton>0) {
-    digitalWrite(PWM_pin, HIGH); 
+  while(millis() - startTime < duration_ms) {
+    unsigned long t0 = micros(); 
+
+    digitalWrite(pin, HIGH); 
     while(micros() - t0 < ton) {}
-  }
 
-  digitalWrite(PWM_pin, LOW); 
-  while(micros() - t0<PWM_period_us){}
+    digitalWrite(pin, LOW); 
+    while(micros() - t0 < PWM_period_us) {}
+  }
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int duty = 0; duty <= steps; duty++) {
-    unsigned long stepTime = millis(); 
+  unsigned long stepTime = SAW_period_ms / steps; 
 
-    while (millis() - stepTime < Step_time_ms) {
-      pwmFixed(duty); 
-    }
+  for (int duty = 0; duty <= 100; duty++) {
+    pwmSoft(PWM_pin, duty, stepTime); 
   }
 }
