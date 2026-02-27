82d84de726dcf9a44fede73fe15bf85e21d45b3a
ANRSchneider <zookeeperana@gmail.com>
2026-02-27 16:33:06 -0600

    Full Lab 2

--- students/Ana/Lab_2_PWM/Lab_2_PWM_Part_1/Lab_2_PWM_Part_1.ino
+++ students/Ana/Lab_2_PWM/Lab_2_PWM_Part_1/Lab_2_PWM_Part_1.ino
@@ -4,7 +4,6 @@
 // PWM function
 
 const int PIN = 13;
-double val;
 
 /* function: pwm
  *  
@@ -24,10 +23,9 @@ void pwm(int PIN, double duty_cycle, double duration, double period){
   double Ton;  // using double precision to avoid overflow
   double Toff;
 
-  Ton = (duty_cycle * period) / 200;  //duty cycle = Ton / total period * 100%
+  Ton = (duty_cycle * period) / 255;  //duty cycle = Ton / total period * 100%
   Toff = period - Ton;
   duration = 100 * 1000;  //100ms in us will turn on/off with certain duty cycle
-  val = duration / period;
 
   // Turn on
   digitalWrite(PIN, HIGH);
@@ -42,23 +40,15 @@ void pwm(int PIN, double duty_cycle, double duration, double period){
 }
 
 void setup() {
-  Serial.begin(9600);
   pinMode(PIN, OUTPUT);
 }
 
 void loop() {
+  double duty_cycle = 125;  //fraction of time that signal spends in active state
+  double period = 10 * 1000;  //want 10 ms
+  double duration;
 
-  for (double i = 0; i < 200; i++) {
-    double duty_cycle = i;  //fraction of time that signal spends in active state
-    duty_cycle = constrain(duty_cycle, 0, 200);
-    //Serial.println(duty_cycle);
-  
-    double period = 10 * 1000;  //want 10 ms
-    double duration = 0.00078; //100 * 1000; //100 ms, this is how long so many periods will run
-
-    pwm(PIN, duty_cycle, duration, period);
-  }
-  //double duty_cycle = 0;
+  pwm(PIN, duty_cycle, duration, period);
 /*  
   // Set yLED brightness
   analogWrite(yLED, brightness);
