c4a87490feeaf99a8bacba0310319d885be4e07c
tsnyder0 <thiasnyder@gmail.com>
2026-04-01 12:45:04 -0500

    Distance Serial Print (Complete?)

--- students/Thea/Lab5/distance_sensor/distance_sensor.ino
+++ students/Thea/Lab5/distance_sensor/distance_sensor.ino
@@ -1,22 +1,20 @@
 //Analog Sensor DISTANCE - serial print
 //Lab 5
 
-char data;
-int dist;
-
-//int val = 0;             //distance variable 
+const int pin = 0;
+char dist; 
 
 
 void setup() {
+  pinMode(pin, INPUT);
   Serial.begin(9600);       //baud rate
 }
 
 void loop() {
   if (Serial.available()){
-    data = Serial.read();
-    
-    
+    dist = Serial.read();
+    Serial.print("Distance: ");
+    Serial.print(dist);
+    Serial.print("inches");
   }
-
-
 }
