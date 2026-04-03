11d0c3a1bfc45e78867e9daba968e7600cd77cb2
Jstruhar <jackson.struhar@gmail.com>
2026-04-02 23:02:08 -0500

    Updated code: Angle of Tilt

--- students/jacksonstruhar/Lab5/Lab5Part2Accelerometer/Lab5Part2Accelerometer.ino
+++ students/jacksonstruhar/Lab5/Lab5Part2Accelerometer/Lab5Part2Accelerometer.ino
@@ -1,87 +1,62 @@
 
-// Basic demo for accelerometer readings from Adafruit LIS3DH
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-
-// Used for software SPI
 #define LIS3DH_CLK 13
 #define LIS3DH_MISO 12
 #define LIS3DH_MOSI 11
-// Used for hardware & software SPI
 #define LIS3DH_CS 10
 
-// software SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
-// hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
-// Low Power 5Khz data rate needs faster SPI, and calling setPerformanceMode & setDataRate
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, 2000000);
-// I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+const int yellowLED = 3;
+const int greenLED = 2;
 
 void setup(void) {
   Serial.begin(115200);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
 
   Serial.println("LIS3DH test!");
 
-  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+  if (! lis.begin(0x18)) {   //i2c address
     Serial.println("Couldnt start");
-    while (1) yield();
+    while (1);
   }
   Serial.println("LIS3DH found!");
-
-  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
-
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());
-  Serial.println("G");
-
-  // lis.setPerformanceMode(LIS3DH_MODE_LOW_POWER);
-  Serial.print("Performance mode set to: ");
-  switch (lis.getPerformanceMode()) {
-    case LIS3DH_MODE_NORMAL: Serial.println("Normal 10bit"); break;
-    case LIS3DH_MODE_LOW_POWER: Serial.println("Low Power 8bit"); break;
-    case LIS3DH_MODE_HIGH_RESOLUTION: Serial.println("High Resolution 12bit"); break;
-  }
-
-  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
-  Serial.print("Data rate set to: ");
-  switch (lis.getDataRate()) {
-    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
-    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
-    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
-    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
-    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
-    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
-
-    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
-    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
-    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("1.6 Khz Low Power"); break;
-  }
 }
 
-void loop() {
-  lis.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  Serial.print("X:  "); Serial.print(lis.x);
-  Serial.print("  \tY:  "); Serial.print(lis.y);
-  Serial.print("  \tZ:  "); Serial.print(lis.z);
 
-  /* Or....get a new sensor event, normalized */
+void loop() {
+ 
   sensors_event_t event;
   lis.getEvent(&event);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
-  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
-  Serial.println(" m/s^2 ");
+  float ax = event.acceleration.x; //xyz values
+  float ay = event.acceleration.y;
+  float az = event.acceleration.z;
 
-  Serial.println();
+  float tilt = atan(sqrt(ax*ax + ay*ay) / az) * 180.0 / PI; //tangent theta equation for 3D space
+
+  Serial.print("Angle of Tilt: "); 
+  Serial.print(tilt);
+  Serial.println(" Degrees");
+
+  //LED values
+  if (tilt <= 1) {
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+  }
+  else if (tilt <= 5) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+  }
+  else {
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+  }
 
   delay(200);
 }
