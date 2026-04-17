1201c242f1c42a50200c03b909a46e8f1b72ef41
ANRSchneider <zookeeperana@gmail.com>
2026-04-17 12:26:54 -0500

    Working angle reading

--- students/Ana/ds1307Lab6/ds1307Lab6.ino
+++ students/Ana/ds1307Lab6/ds1307Lab6.ino
@@ -2,10 +2,19 @@
 #include "RTClib.h"
 #include <SPI.h>
 #include <SD.h>
+#include <Wire.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
 
 //set up variables from RCT library
 RTC_DS1307 rtc;
-// set up variables using the SD utility library functions:
+// Used for software SPI and acc
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 const int chipSelect = 10;
 
@@ -16,9 +25,6 @@ void setup () {
   Serial.begin(9600);
   pinMode(0, INPUT);
 
-// #ifndef ESP8266
-//   while (!Serial); // wait for serial port to connect. Needed for native USB
-// #endif
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
@@ -37,14 +43,12 @@ void setup () {
 
   Serial.println("initialization done.");
 
-  
+  //initialization of RTC
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     while (1) delay(10);
   }
-
-  
 
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running, let's set the time!");
@@ -65,6 +69,16 @@ void setup () {
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("LIS3DH test!");
+
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
 }
 
 void loop () {
@@ -98,8 +112,19 @@ void loop () {
     delayMicroseconds(1);
   }
   int nbytes = Serial.readBytes(dist, 3);
-  Serial.println(dist);
+  // Serial.println(dist);
 
+
+  //Angle Sensor
+  sensors_event_t event;
+  lis.getEvent(&event);
+
+  double angle_radian = atan(sqrt( sq(event.acceleration.x) + sq(event.acceleration.y) ) / event.acceleration.z);
+  double angle = angle_radian * (180 / 3.13);
+  Serial.println(angle);
+
+
+  //Writing to the SD card
   String dataString = "";
 
   // read three sensors and append to the string:
