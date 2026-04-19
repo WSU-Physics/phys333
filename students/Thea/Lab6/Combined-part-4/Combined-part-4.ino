f724e53ae5f3b43e88739d33437855260dc7269b
tsnyder0 <thiasnyder@gmail.com>
2026-04-19 09:53:15 -0500

    Combined Design 2nd Attempt (before Verify and Upload)
    The separated designs were copy and pasted into this singular design. Everything was included in this attempt, in their respective sections: preamble, setup, and loop. I put them in the order: Distance Sensor (3, 50, 154), Data Logger (7, 54, 167), Accelerometer (13, 73, 196), and Date & Time (38, 117, 271).

--- students/Thea/Lab6/Combined-part-4/Combined-part-4.ino
+++ students/Thea/Lab6/Combined-part-4/Combined-part-4.ino
@@ -1,49 +1,65 @@
 /* Lab 6 combine all pieces for part 4*/
 
+/*----Distance Sensor---*/
+const int pin = 0;
+char dist[4];
+
+/*---Data Logger---*/
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 10;
+
+/*---Accelerometer---*/
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-#include "RTClib.h"
-#include <SPI.h>
-#include <SD.h>
 
-/* -------------Devices-------------- */
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// software SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// Low Power 5Khz data rate needs faster SPI, and calling setPerformanceMode & setDataRate
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, 2000000);
+// I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
-RTC_DS1307 rtc;
-// SD card chip 
-const int chipSelect = 10;
-//distance
-const int pin = 0;
-char dist[4];
 
-// CSV
-const char *filename = "lab6-scan.csv";
+const int YLED = 9;
+const int GLED = 10;
+
+/*---Date and Time---*/
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 /* ------ Set Up----------*/
+
 void setup() {
-  Serial.begin(115200);
-  while(!Serial);
 
-  // Accelerometer
-  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
-    Serial.println("Couldnt start");
-    while (1) yield();
-  }
-  Serial.println("LIS3DH found!");
+/*---Distance Sensor---*/
+  pinMode(pin, INPUT);
+  Serial.begin(9600);       //baud rate
 
-  // RTC
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
-  // SD Card
-   if (!SD.begin(chipSelect)) {
+/*---Data Logger---*/
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // wait for Serial Monitor to connect. Needed for native USB port boards only:
+  while (!Serial);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed. Things to check:");
     Serial.println("1. is a card inserted?");
     Serial.println("2. is your wiring correct?");
@@ -52,39 +68,90 @@ void setup() {
     while (true);
   }
 
-  // CSV File
-  File dataFile = SD.open(filename, FILE_WRITE);
+  Serial.println("initialization done.");
 
-      // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println("year,month,day,hour,minute,second,distance_in,ax,ay,az,roll,pitch");
-    dataFile.close();
+/*---Accelerometer---*/  
+  Serial.begin(115200);
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("LIS3DH test!");
+
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
   }
-      // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
+  Serial.println("LIS3DH found!");
+
+  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+  Serial.println("G");
+
+  // lis.setPerformanceMode(LIS3DH_MODE_LOW_POWER);
+  Serial.print("Performance mode set to: ");
+  switch (lis.getPerformanceMode()) {
+    case LIS3DH_MODE_NORMAL: Serial.println("Normal 10bit"); break;
+    case LIS3DH_MODE_LOW_POWER: Serial.println("Low Power 8bit"); break;
+    case LIS3DH_MODE_HIGH_RESOLUTION: Serial.println("High Resolution 12bit"); break;
   }
+
+  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+  Serial.print("Data rate set to: ");
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+
+    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("1.6 Khz Low Power"); break;
+  }
+
+  pinMode (YLED, OUTPUT);
+  pinMode (GLED, OUTPUT);
+
+/*---Date and Time---*/
+  Serial.begin(57600);
+
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 } 
+
+#define DEBUG 0 //enable or disable printing 
 
 /*-------- Loop ---------*/
 
 void loop() {
-  // RTC
-  DateTime now = rtc.now();
 
-  // Accelerometer
-  lis.read();
-  sensors_event_t event;
-  lis.getEvent(&event);
-
-  float Ax = event.acceleration.x;
-  float Ay = event.acceleration.y;
-  float Az = event.acceleration.z;
-
-  float roll = atan2(Ay, Az) * 180 / PI;
-  float pitch = atan2(-Ax, sqrt(Ay * Ay + Az * Az)) * 180 / PI;
-
-  // Distance
+/*---Distance Sensor---*/
   while(Serial.available()){Serial.read();}
   while(Serial.read() !=82){
     delayMicroseconds(1000);
@@ -92,30 +159,156 @@ void loop() {
   }
   int nbytes = Serial.readBytes(dist,3);
 
-/* ------- Write onto SD Card ---------*/
-  File dataFile = SD.open(filename, FILE_WRITE);
+    Serial.println(" ");
+    //Serial.print("Distance: ");
+    Serial.print(dist);
+    //Serial.print(" inches");
 
+/*---Data Logger---*/
+ // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
   if (dataFile) {
-    dataFile.print(now.year());   dataFile.print(",");
-    dataFile.print(now.month());  dataFile.print(",");
-    dataFile.print(now.day());    dataFile.print(",");
-    dataFile.print(now.hour());   dataFile.print(",");
-    dataFile.print(now.minute()); dataFile.print(",");
-    dataFile.print(now.second()); dataFile.print(",");
-
-    dataFile.print(dist);         dataFile.print(",");
-    dataFile.print(Ax, 3);        dataFile.print(",");
-    dataFile.print(Ay, 3);        dataFile.print(",");
-    dataFile.print(Az, 3);        dataFile.print(",");
-    dataFile.print(roll, 2);      dataFile.print(",");
-    dataFile.println(pitch, 2);
-
+    dataFile.println(dataString);
     dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
   }
-  else{
-    Serial.println("Error for CSV file");
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
   }
 
+/*---Accelerometer---*/
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  #if DEBUG
+  Serial.print("X:  "); Serial.print(lis.x);
+  Serial.print("  \tY:  "); Serial.print(lis.y);
+  Serial.print("  \tZ:  "); Serial.print(lis.z);
+  #endif
+
+  /* Or....get a new sensor event, normalized */
+  sensors_event_t event;
+  lis.getEvent(&event);
+
+
+  #if DEBUG
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  Serial.println();
+  #endif
+
+  float Ax = event.acceleration.x;
+  float Ay = event.acceleration.y;
+  float Az = event.acceleration.z;
+
+  float roll = atan2(Ay, Az) * 180 / PI;
+  float pitch = atan2(-Ax, sqrt(Ay * Ay + Az * Az)) * 180 / PI;
+  
+  #if DEBUG
+  Serial.print("\t\tRoll: "); Serial.print(roll);       //tilt along Y-axis
+  Serial.print(" \tPitch: "); Serial.print(pitch);     //tilt along X-axis
+
+  Serial.println();
+  #endif
+
+  // Serial Plotter output
+  Serial.print("X:");
+  Serial.print(Ax);
+  Serial.print(" ");
+
+  Serial.print("Y:");
+  Serial.print(Ay);
+  Serial.print(" ");
+
+  Serial.print("Z:");
+  Serial.print(Az);
+  //Serial.println(Az);
+  Serial.print(" ");
+
+  Serial.print("Roll:");
+  Serial.print(roll);
+  Serial.print(" ");
+
+  Serial.print("Pitch:");
+  Serial.println(pitch);
+
+  #if DEBUG
+  if ((roll > -2.5 && roll < 2.5) || (pitch > -2.5 && pitch < 2.5)){
+    analogWrite(GLED, 128);
+  } else {
+    analogWrite(GLED, 0);
+  }
+
+  if ((roll > 2.5 && roll < 10) || (roll > -10 && roll < -2.5) || (pitch > 2.5 && pitch < 10) || (pitch > -10 && pitch < -2.5)){
+    analogWrite(YLED, 128);
+  } else {
+    analogWrite(YLED, 0);
+  }
+  #endif
+  
   delay(200);
 
+/*---Date and Time---*/
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // calculate a date which is 7 days, 12 hours, 30 minutes, and 6 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print(" now + 7d + 12h + 30m + 6s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+
+    Serial.println();
+    delay(3000);
 }
