365883c692aad744dacfa11b5ae75838b02c4537
lanewaller13 <143553769+lanewaller13@users.noreply.github.com>
2026-04-30 00:25:11 -0500

    update jump height calculation
    Added comments for better code understanding and incorporated the overall jump height estimation calculation that could be derived from flight time.

--- students/Lane_Waller/finalProject-flightTime_jumpHeight_tester/finalProject-flightTime_jumpHeight_tester.ino
+++ students/Lane_Waller/finalProject-flightTime_jumpHeight_tester/finalProject-flightTime_jumpHeight_tester.ino
@@ -1,24 +1,22 @@
-//Use a Hardware-Debounced Switch to Control an Interrupt
-
-//Button pins             //Pin 2 has hardware interrupt capability thus will use this pin for sensor reading input
-const int IR_sensor = 2;  //Output of Infrared break beam laser sensor on pin 2
-
+//IR Sensor Pin declaration    //Pin 2 has hardware interrupt capability thus will use this pin for sensor reading input
+const int IR_sensor = 2;       //Output of Infrared break beam laser sensor on pin 2
 
 //Volatile variables can change inside interrupts
-volatile bool jump_complete = false;
-volatile unsigned long toeOff_time = 0;
-volatile unsigned long return_time = 0;
+volatile bool jump_complete = false;         //jump recorded status variable
+volatile unsigned long toeOff_time = 0;      //start time: beginning of jumo (beam goes from broken to intact)
+volatile unsigned long return_time = 0;      //end time: end of sensed jump (beam returns to broken state)
 
 void setup()
 {
   pinMode(IR_sensor, INPUT_PULLUP);
-  //The pin is inverted, so we want to look at the rising edge
-  attachInterrupt(digitalPinToInterrupt(IR_sensor), beamChange, CHANGE); //which pin to attach the intterupt to, what ISR will be called, when will it be called
+  
+  /*Attach interrupt setup: 
+        which pin to attach the intterupt to, what ISR will be called, when will it be called*/
+  attachInterrupt(digitalPinToInterrupt(IR_sensor), beamChange, CHANGE); 
 }
 
 void beamChange()
 {
-  
   //ISR has been activated, read the sensor and time when beam is not broken
   if (digitalRead(IR_sensor) == LOW) {
     toeOff_time = micros();
@@ -27,18 +25,22 @@ void beamChange()
     return_time = micros();   //record time when beam returns to broken state
     jump_complete = true;     //switch jump variable to mark a complete jump
   }
-    
 }
 
 void loop()
 {
   if(jump_complete = true){
-  //output flight time reading:
-  unsigned long flightTime = (return_time - toeOff_time)/1000000; //convert microseconds to seconds
+  //output flight time reading and Jump height:
+  unsigned long flightTime = (return_time - toeOff_time)/1000000;          //convert microseconds to seconds
+  unsigned long jumpHeight_meters = (9.80665*flightTime*flightTIme) / 8    //Bosco Protocol formula outputs jump height (in m)
+  unsigned long jumpHeight_inches = jumpHeight_meters*39.37007874          //meters to inches conversion
   Serial.print("Flight time: ");
   Serial.print(flightTime);
   Serial.println(" seconds");
-  jump_complete = false;                    //return variable to false so another jump may be tested
+  Serial.print("Jump height: ");
+  Serial.print(jumpHeight_inches);
+  Serial.println(" inches");
+  jump_complete = false;                //return variable to false so another jump may be tested
   }
 
   else { }
