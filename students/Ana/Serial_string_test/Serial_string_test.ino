ad440722115df6bf08982b269ce38f9303d310ad
ANRSchneider <zookeeperana@gmail.com>
2026-05-04 23:16:39 -0500

    Button Testing

--- students/Ana/Serial_string_test/Serial_string_test.ino
+++ students/Ana/Serial_string_test/Serial_string_test.ino
@@ -3,14 +3,33 @@
 
 String in_bytes;
 String prev_song;
+const int BUTTON = 2;
+
+boolean lastButton = LOW;
+boolean currentButton = LOW;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT);
 
 }
 
+boolean debounce(boolean last) {
+  boolean current = digitalRead(BUTTON);
+  if(last != current) {
+    delay(5);
+    current = digitalRead(BUTTON);
+  }
+  return current;
+}
+
 void loop() {
+  currentButton = debounce(lastButton);
+  if (lastButton == LOW && currentButton == HIGH) {
+    Serial.println("gwa");
+  }
+  lastButton = currentButton;
+
   if(Serial.available() > 0) {  //is equal to -1 if not data I think
     in_bytes = Serial.readStringUntil('\n');  //read until new line
     // Serial.write("current song");
